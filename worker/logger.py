77af65977a977a453266c7534700e5890d9cb07d
Oleksii Symon <oleksii.symon@corva.ai>
2020-11-10 15:31:31 +0200

    Added logger.py

---
+++ worker/logger.py
@@ -0,0 +1,58 @@
+from logging import LoggerAdapter, Formatter, getLogger
+from logging.config import dictConfig
+from worker.settings import LOG_LEVEL
+from time import gmtime
+
+
+class UtcFormatter(Formatter):
+    converter = gmtime
+
+
+dictConfig(
+    {
+        'version': 1,
+        'formatters': {
+            'default': {
+                '()': UtcFormatter,
+                'format': '%(asctime)s %(name)-5s %(levelname)-5s %(message)s'
+            }
+        },
+        'handlers': {
+            'stream': {
+                'class': 'logging.StreamHandler',
+                'level': LOG_LEVEL,
+                'formatter': 'default',
+                'stream': 'ext://sys.stdout'
+            }
+        },
+        'loggers': {
+            'main': {
+                'level': LOG_LEVEL,
+                'handlers': ['stream'],
+                'propagate': False
+            }
+        }
+    }
+)
+
+
+class LogAdapter(LoggerAdapter):
+    extra_fields = []
+
+    def process(self, msg, kwargs):
+        message_parts = [
+            f'{field}:{self.extra[field]}     '
+            for field in self.extra_fields
+            if field in self.extra
+        ]
+        message_parts[-1] = f'{message_parts[-1].strip()}:     '
+        message_parts.append(str(msg))
+        message = ' '.join(message_parts)
+        return message, kwargs
+
+
+class AppLogger(LogAdapter):
+    extra_fields = ['asset_id']
+
+
+LOGGER = getLogger('main')
