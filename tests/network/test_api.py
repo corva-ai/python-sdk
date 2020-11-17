58c2575098bc1a76fba71ec949791c847319b0a9
Oleksii Symon <oleksii.symon@corva.ai>
2020-11-17 16:06:20 +0200

    Fixed data api regex in api

--- tests/network/test_api.py
+++ tests/network/test_api.py
@@ -9,7 +9,7 @@ from worker.network.api import Api
 
 @fixture
 def api():
-    return Api(api_url='https://localhost:12345/random', data_api_url='https://localhost:12345/random')
+    return Api(api_url='https://api', data_api_url='https://data')
 
 
 def test_default_headers(api):
@@ -38,14 +38,14 @@ def test_get_url(api):
     path = f'{api.data_api_url}/1'
     assert api._get_url(path=path) == path
 
-    path = '/v10/data'
+    path = '/api/v10/data'
     assert api._get_url(path=path) == f'{api.data_api_url}{path}'
 
-    path = 'v10/data'
+    path = 'api/v10/data'
     assert api._get_url(path=path) == f'{api.data_api_url}/{path}'
 
-    path = 'v10/message_producer'
+    path = 'api/v10/message_producer'
     assert api._get_url(path=path) == f'{api.data_api_url}/{path}'
 
-    path = '/v10/path'
-    assert api._get_url(path=path) == f'{api.api_url}{path}'
+    path = 'api/v10/path'
+    assert api._get_url(path=path) == f'{api.api_url}/{path}'
