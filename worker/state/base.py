9067549340d8c3ec46be2c8a5a43cd8810be612f
Oleksii Symon <oleksii.symon@corva.ai>
2020-11-19 19:57:41 +0200

    added BaseState class

---
+++ worker/state/base.py
@@ -0,0 +1,16 @@
+from abc import ABC, abstractmethod
+from typing import List
+
+
+class BaseState(ABC):
+    @abstractmethod
+    def load(self, state_key: str) -> dict:
+        ...
+
+    @abstractmethod
+    def save(self, state: dict, state_key: str) -> None:
+        ...
+
+    @abstractmethod
+    def delete(self, state_keys: List[str]) -> int:
+        ...
