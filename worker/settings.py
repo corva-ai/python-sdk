def1d61c0abd771e9218b679068f4c040c8e1d21
Oleksii Symon <oleksii.symon@corva.ai>
2020-11-19 20:31:27 +0200

    added APP_KEY to settings.py

--- worker/settings.py
+++ worker/settings.py
@@ -3,6 +3,7 @@ from typing import Final
 
 API_ROOT_URL: Final = os.getenv('API_ROOT_URL', '#SETME')
 DATA_API_ROOT_URL: Final = os.getenv('DATA_API_ROOT_URL', '#SETME')
+APP_KEY: Final[str] = os.getenv('APP_KEY', '#SETME')
 APP_NAME: Final = os.getenv('APP_NAME', '#SETME')
 API_KEY: Final = os.getenv('API_KEY', '#SETME')
 CACHE_URL: Final[str] = os.getenv('CACHE_URL')
