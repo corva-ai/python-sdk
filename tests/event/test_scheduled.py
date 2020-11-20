292d4eeba89be5f51054802b56e1ff6da74a666d
Oleksii Symon <oleksii.symon@corva.ai>
2020-11-20 13:12:53 +0200

    updated added tests for ScheduledEvent

---
+++ tests/event/test_scheduled.py
@@ -0,0 +1,12 @@
+from worker.event.scheduled import ScheduledEvent
+
+SCHEDULED_EVENT_FILE_PATH = 'data/tests/scheduled_event.json'
+
+
+def test_load():
+    """test that sample scheduled event loaded without exceptions"""
+
+    with open(SCHEDULED_EVENT_FILE_PATH) as scheduled_event:
+        event_str = scheduled_event.read()
+    event = ScheduledEvent.load(event=event_str)
+    assert len(event) == 2
