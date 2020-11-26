aad2b99f59cf68feafbaf93a73ee8a982ef37573
Oleksii Symon <oleksii.symon@corva.ai>
2020-11-26 14:28:28 +0200

    added test_get_state_key in tests/event/test_scheduled.py

--- tests/event/test_scheduled.py
+++ tests/event/test_scheduled.py
@@ -6,3 +6,11 @@ def test_load(scheduled_event_str):
 
     event = ScheduledEvent.load(event=scheduled_event_str)
     assert len(event) == 3
+
+
+def test_get_state_key(scheduled_event_str):
+    provider = 'provider'
+    app_key = f'{provider}.app-key'
+    state_key = ScheduledEvent.get_state_key(event=scheduled_event_str, app_key=app_key)
+    expected = f'{provider}/well/{39293110}/stream/{11792}/{app_key}/{269616}'
+    assert state_key == expected
