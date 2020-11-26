48224dd562f385f5c8059612e814a96c5ce0b72d
Oleksii Symon <oleksii.symon@corva.ai>
2020-11-26 15:06:43 +0200

    fixed test_get_state_key in tests/event/test_scheduled.py

--- tests/event/test_scheduled.py
+++ tests/event/test_scheduled.py
@@ -11,6 +11,9 @@ def test_load(scheduled_event_str):
 def test_get_state_key(scheduled_event_str):
     provider = 'provider'
     app_key = f'{provider}.app-key'
+    asset_id = 39293110
+    app_stream_id = 11792
+    app_connection_id = 269616
     state_key = ScheduledEvent.get_state_key(event=scheduled_event_str, app_key=app_key)
-    expected = f'{provider}/well/{39293110}/stream/{11792}/{app_key}/{269616}'
+    expected = f'{provider}/well/{asset_id}/stream/{app_stream_id}/{app_key}/{app_connection_id}'
     assert state_key == expected
