187ea9584ff6dd83e1330aa2047ba52a346ee50b
Oleksii Symon <oleksii.symon@corva.ai>
2020-11-26 15:15:07 +0200

    Added StreamEventData

---
+++ corva/event/data/stream.py
@@ -0,0 +1,22 @@
+from typing import Any, Dict, List, Optional
+
+from corva.event.data.base import BaseEventData
+
+
+class Record(BaseEventData):
+    timestamp: int
+    asset_id: int
+    company_id: int
+    version: int
+    data: Dict[str, Any]
+    measured_depth: Optional[float] = None
+    collection: Optional[str] = None
+
+
+class StreamEventData(BaseEventData):
+    records: List[Record]
+    metadata: Dict[str, Any]
+    asset_id: int
+    app_connection_id: int
+    app_stream_id: int
+    is_completed: bool
