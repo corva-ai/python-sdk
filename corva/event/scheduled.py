365aba511e499d9ad0bc2123ba0c47a1bda631da
Oleksii Symon <oleksii.symon@corva.ai>
2020-11-26 13:51:27 +0200

    Fix after merge.

--- corva/event/scheduled.py
+++ corva/event/scheduled.py
@@ -3,14 +3,13 @@ from __future__ import annotations
 from itertools import chain
 from typing import List
 
-from corva.constants import RAW_EVENT_TYPE
 from corva.event.base import BaseEvent
 from corva.event.data.scheduled import ScheduledEventData
 
 
 class ScheduledEvent(BaseEvent):
     @classmethod
-    def load(cls, event: RAW_EVENT_TYPE, **kwargs) -> ScheduledEvent:
+    def load(cls, event: str, **kwargs) -> ScheduledEvent:
         event: List[List[dict]] = super()._load(event=event)
         data = [
             ScheduledEventData(**subdata)
