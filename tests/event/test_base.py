369fbb4b9db15558392081f83224ad3a81f68a9f
Oleksii Symon <oleksii.symon@corva.ai>
2020-11-27 13:38:17 +0200

    fixed BaseEvent tests

--- tests/event/test_base.py
+++ tests/event/test_base.py
@@ -84,19 +84,3 @@ def test_abstractmethods():
     assert getattr(BaseEvent, '__abstractmethods__') == frozenset(['load'])
     with pytest.raises(TypeError):
         BaseEvent(data=[])
-
-
-def test_get_state_key(patch_base_event):
-    provider = 'provider'
-    app_key = f'{provider}.app-key'
-    asset_id = 1
-    app_stream_id = 2
-    app_connection_id = 3
-
-    event = BaseEvent(
-        data=[BaseEventData(asset_id=asset_id, app_stream_id=app_stream_id, app_connection_id=app_connection_id)]
-    )
-
-    with patch.object(BaseEvent, 'load', return_value=event):
-        state_key = BaseEvent.get_state_key(event='', app_key=app_key)
-        assert state_key == f'{provider}/well/{asset_id}/stream/{app_stream_id}/{app_key}/{app_connection_id}'
