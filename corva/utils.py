2a80fe047326d947cef809d4279bf889130669dc
Oleksii Symon <oleksii.symon@corva.ai>
2020-12-04 12:28:05 +0200

    Added GetStateKey convenience class to utils.py

--- corva/utils.py
+++ corva/utils.py
@@ -1,8 +1,22 @@
-def get_state_key(asset_id: int, app_stream_id: int, app_key: str, app_connection_id: int):
-    provider = get_provider(app_key=app_key)
-    state_key = f'{provider}/well/{asset_id}/stream/{app_stream_id}/{app_key}/{app_connection_id}'
-    return state_key
+from corva.event.event import Event
 
 
-def get_provider(app_key: str) -> str:
-    return app_key.split('.')[0]
+class GetStateKey:
+    @classmethod
+    def _get_key(cls, asset_id: int, app_stream_id: int, app_key: str, app_connection_id: int):
+        provider = cls._get_provider(app_key=app_key)
+        state_key = f'{provider}/well/{asset_id}/stream/{app_stream_id}/{app_key}/{app_connection_id}'
+        return state_key
+
+    @staticmethod
+    def _get_provider(app_key: str) -> str:
+        return app_key.split('.')[0]
+
+    @classmethod
+    def from_event(cls, event: Event, app_key: str):
+        return cls._get_key(
+            asset_id=event[0].asset_id,
+            app_stream_id=event[0].app_stream_id,
+            app_key=app_key,
+            app_connection_id=event[0].app_connection_id
+        )
