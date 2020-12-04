2b25a8dc1fdee8d03995389e6482c4a625fb00ef
Oleksii Symon <oleksii.symon@corva.ai>
2020-12-04 12:23:28 +0200

    Added Event class that should replace all other event classes

---
+++ corva/event/event.py
@@ -0,0 +1,21 @@
+from __future__ import annotations
+
+from collections import UserList
+from typing import List
+
+from corva.event.data.base import BaseEventData
+
+
+class Event(UserList):
+    def __init__(self, data: List[BaseEventData]):
+        super().__init__(data)
+
+    def __eq__(self, other):
+        return (
+             super().__eq__(other)
+             and
+             all(
+                 type(self_value) == type(other_value)
+                 for self_value, other_value in zip(self, other)
+             )
+        )
