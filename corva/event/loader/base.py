e5413ee13bb5f375b8f9f70a39f3b22cf21d527a
Oleksii Symon <oleksii.symon@corva.ai>
2020-12-04 12:24:16 +0200

    Added BaseLoader

---
+++ corva/event/loader/base.py
@@ -0,0 +1,28 @@
+from __future__ import annotations
+
+import json
+from abc import abstractmethod, ABC
+
+from corva.constants import EVENT_TYPE
+from corva.event.event import Event
+
+
+class BaseLoader(ABC):
+    @abstractmethod
+    def load(self, event: str) -> Event:
+        pass
+
+    @staticmethod
+    def _load_json(event: str) -> EVENT_TYPE:
+        if not event:
+            raise ValueError('Empty event')
+
+        if not isinstance(event, str):
+            raise TypeError(f'Unknown event type {type(event)}')
+
+        try:
+            event = json.loads(event)
+        except ValueError as exc:
+            raise ValueError('Invalid event JSON') from exc
+
+        return event
