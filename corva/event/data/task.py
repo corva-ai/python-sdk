034b8983828e0be69c57a238b1349229bbb14b8f
Oleksii Symon <oleksii.symon@corva.ai>
2020-12-07 13:26:55 +0200

    added version restriction to TaskEventData

--- corva/event/data/task.py
+++ corva/event/data/task.py
@@ -1,9 +1,11 @@
 from typing import Optional
 
+from pydantic import conint
+
 from corva.event.data.base import BaseEventData
 
 
 class TaskEventData(BaseEventData):
     id: Optional[str] = None
     task_id: str
-    version: int
+    version: conint(ge=2, le=2)  # only utils API v2 supported
