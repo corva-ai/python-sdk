b1d8d7a737461720a20466051ed44e9288240887
Oleksii Symon <oleksii.symon@corva.ai>
2020-12-07 15:17:58 +0200

    updated some tests in loader/test_scheduled.py and loader/test_stream.py

--- tests/event/loader/test_scheduled.py
+++ tests/event/loader/test_scheduled.py
@@ -1,3 +1,4 @@
+from corva.event.event import Event
 from corva.event.loader.scheduled import ScheduledLoader
 
 
@@ -5,4 +6,6 @@ def test_load(scheduled_event_str):
     """test that sample scheduled event loaded without exceptions"""
 
     event = ScheduledLoader().load(event=scheduled_event_str)
+
     assert len(event) == 3
+    assert isinstance(event, Event)
