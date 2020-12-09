d974bf69737cbb21a4833153947fbeec414139bb
Oleksii Symon <oleksii.symon@corva.ai>
2020-12-09 18:17:01 +0200

    deleted custom __eq__ in Event

--- corva/event/event.py
+++ corva/event/event.py
@@ -9,13 +9,3 @@ from corva.event.data.base import BaseEventData
 class Event(UserList):
     def __init__(self, data: List[BaseEventData]):
         super().__init__(data)
-
-    def __eq__(self, other):
-        return (
-             super().__eq__(other)
-             and
-             all(
-                 type(self_value) == type(other_value)
-                 for self_value, other_value in zip(self, other)
-             )
-        )
