d7b4317e516e214a38f3b82cec28adf1904f4f19
Oleksii Symon <oleksii.symon@corva.ai>
2020-12-11 13:36:09 +0200

    fix: max_retries are not set back after overriding in Api._request

--- corva/network/api.py
+++ corva/network/api.py
@@ -1,6 +1,7 @@
 import os
 import re
 from typing import List, Optional
+from unittest.mock import patch
 
 from requests import Response, Session
 from requests.adapters import HTTPAdapter
@@ -96,16 +97,15 @@ class Api:
         timeout = timeout or self.timeout
 
         # not thread safe
-        self.session.adapters['https://'].max_retries.total = max_retries
-
-        response = self.session.request(
-            method=method,
-            url=self._get_url(path=path),
-            params=params,
-            json=data,
-            headers=headers,
-            timeout=timeout
-        )
+        with patch.object(self.session.adapters['https://'].max_retries, 'total', max_retries):
+            response = self.session.request(
+                method=method,
+                url=self._get_url(path=path),
+                params=params,
+                json=data,
+                headers=headers,
+                timeout=timeout
+            )
 
         response.raise_for_status()
 
