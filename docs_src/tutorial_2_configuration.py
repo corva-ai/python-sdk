e61af57aa290aa3bfbba0b14241f27618b8d2a36
Oleksii Symon <oleksii.symon@corva.ai>
2020-12-23 15:58:05 +0200

    moved config params from Config class to Corva.__init__ in configuration tutorial

--- docs_src/tutorial_2_configuration.py
+++ docs_src/tutorial_2_configuration.py
@@ -1,30 +1,16 @@
-"""
-idea https://docs.celeryproject.org/en/stable/userguide/application.html#example-3-using-a-configuration-class-object
-"""
-
 from corva import Api, Corva, Event, State
 
-app = Corva()
+app = Corva(
+    # 1 api params
+    api_url='api.localhost',
+    api_data_url='api.data.localhost',
+    api_key='api_key',
+    api_app_name='api_app_name',
 
-
-class Config:
-    # 1 override default parameters for Corva instance
-
-    # 2 api params
-    api_url = 'api.localhost'
-    api_data_url = 'api.data.localhost'
-    api_key = 'api_key'
-    api_app_name = 'api_app_name'
-
-    # 3 state params
-    state_url = 'redis://'
-    state_params = {'param1': 'val1'}
-
-    # 4 other params
-    ...
-
-
-app.config_from_object(Config)
+    # 2 state params
+    state_url='redis://',
+    state_params={'param1': 'val1'}
+)
 
 
 @app.stream
@@ -37,4 +23,4 @@ def user_job(event: Event, api: Api, state: State):
 def lambda_handler(event, context):
     """AWS lambda handler"""
 
-    user_job.run(event)
+    user_job(event)
