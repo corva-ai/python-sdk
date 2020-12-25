4f760e10a8a192fc4cdb1eafe77d72175d7338f0
Oleksii Symon <oleksii.symon@corva.ai>
2020-12-25 12:39:23 +0200

    updated StreamLoader

--- corva/loader/stream.py
+++ corva/loader/stream.py
@@ -1,53 +1,16 @@
-from __future__ import annotations
-
-from typing import List
-
-from corva.event import Event
-from corva.models.stream import StreamEventData
 from corva.loader.base import BaseLoader
+from corva.models.stream import StreamEvent
+from corva.types import STREAM_EVENT_TYPE
 
 
-class StreamLoader(BaseLoader):
+class StreamLoader(BaseLoader[StreamEvent, STREAM_EVENT_TYPE]):
     def __init__(self, app_key: str):
         self.app_key = app_key
 
-    def load(self, event: str) -> Event:
-        event: List[dict] = super()._load_json(event=event)
+    def load(self, event: str) -> StreamEvent:
+        event = super().load(event=event)  # type: StreamEvent
 
-        data = []
-        for subdata in event:
-            asset_id = self.get_asset_id(data=subdata)
-            app_connection_id = self._get_app_connection_id(subdata=subdata, app_key=self.app_key)
-            app_stream_id = subdata['metadata']['app_stream_id']
-            is_completed = self._get_is_completed(records=subdata['records'])
+        for subevent in event:
+            subevent.app_key = self.app_key
 
-            data.append(StreamEventData(
-                asset_id=asset_id,
-                app_connection_id=app_connection_id,
-                app_stream_id=app_stream_id,
-                is_completed=is_completed,
-                **subdata
-            ))
-
-        return Event(data)
-
-    @staticmethod
-    def _get_is_completed(records: List[dict]):
-        try:
-            return records[-1].get('collection') == 'wits.completed'
-        except IndexError as exc:
-            raise ValueError(f'Records are empty: {records}') from exc
-
-    @staticmethod
-    def _get_app_connection_id(subdata: dict, app_key: str):
-        try:
-            return subdata['metadata']['apps'][app_key]['app_connection_id']
-        except KeyError as exc:
-            raise ValueError(f'Can\'t get {app_key} from metadata.apps.') from exc
-
-    @staticmethod
-    def get_asset_id(data: dict):
-        try:
-            return data['records'][0]['asset_id']
-        except (IndexError, KeyError) as exc:
-            raise ValueError(f'Could not find an asset id in data: {data}.') from exc
+        return event
--- corva/models/stream.py
+++ corva/models/stream.py
@@ -1,6 +1,7 @@
-from typing import Any, Dict, List, Optional
+from datetime import datetime
+from typing import Dict, List, Optional
 
-from corva.models.base import BaseContext, BaseEventData
+from corva.models.base import BaseContext, BaseEventData, ListEvent
 from corva.state.redis_state import RedisState
 
 
@@ -8,20 +9,48 @@ class StreamContext(BaseContext):
     state: RedisState
 
 
-class Record(BaseEventData):
-    timestamp: Optional[int] = None
-    asset_id: int
-    company_id: int
-    version: int
-    data: Dict[str, Any]
-    measured_depth: Optional[float] = None
-    collection: str
-
-
 class StreamEventData(BaseEventData):
+    class Record(BaseEventData):
+        class Data(BaseEventData):
+            hole_depth: Optional[float] = None
+            weight_on_bit: Optional[int] = None
+            state: Optional[str] = None
+
+        timestamp: Optional[datetime] = None
+        asset_id: int
+        company_id: int
+        version: int
+        data: Data
+        measured_depth: Optional[float] = None
+        collection: str
+
+    class Metadata(BaseEventData):
+        class AppKeyData(BaseEventData):
+            app_connection_id: int
+
+        app_stream_id: int
+        apps: Dict[str, AppKeyData]
+
+    app_key: Optional[str] = None
     records: List[Record]
-    metadata: Dict[str, Any]
-    asset_id: int
-    app_connection_id: int
-    app_stream_id: int
-    is_completed: bool
+    metadata: Metadata
+
+    @property
+    def asset_id(self) -> int:
+        return self.records[0].asset_id
+
+    @property
+    def app_connection_id(self) -> int:
+        return self.metadata.apps[self.app_key].app_connection_id
+
+    @property
+    def app_stream_id(self) -> int:
+        return self.metadata.app_stream_id
+
+    @property
+    def is_completed(self) -> bool:
+        return self.records[-1].collection == 'wits.completed'
+
+
+class StreamEvent(ListEvent[StreamEventData]):
+    pass
