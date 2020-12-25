6204942e20f16a8e373b04e273364d1303ef855c
Oleksii Symon <oleksii.symon@corva.ai>
2020-12-25 14:11:53 +0200

    lint fix

--- corva/types.py
+++ corva/types.py
@@ -1,4 +1,4 @@
-from typing import Any, Callable, List, Union
+from typing import Any, Callable, Union
 
 REDIS_STORED_VALUE_TYPE = Union[bytes, str, int, float]
 DISPATCH_TYPE = Callable[['BaseContext', Callable[['BaseContext'], Any]], Any]  # noqa: F821
