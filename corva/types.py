63cfc4f473b6ea6e4b02c605a1421015bd483710
Oleksii Symon <oleksii.symon@corva.ai>
2020-12-25 14:11:13 +0200

    deleted not used type constants

--- corva/types.py
+++ corva/types.py
@@ -1,7 +1,4 @@
 from typing import Any, Callable, List, Union
 
 REDIS_STORED_VALUE_TYPE = Union[bytes, str, int, float]
-SCHEDULED_EVENT_TYPE = List[List[dict]]
-STREAM_EVENT_TYPE = List[dict]
-TASK_EVENT_TYPE = dict
 DISPATCH_TYPE = Callable[['BaseContext', Callable[['BaseContext'], Any]], Any]  # noqa: F821
