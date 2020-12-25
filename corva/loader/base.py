eecbaf4e045cf63e1cf8a84bb97f1941ee2a0780
Oleksii Symon <oleksii.symon@corva.ai>
2020-12-25 13:35:43 +0200

    reworked BaseLoader

--- corva/loader/base.py
+++ corva/loader/base.py
@@ -1,14 +1,18 @@
-from typing import Generic, TypeVar
+from abc import ABC, abstractmethod
+from typing import Any, ClassVar
 
 from pydantic import parse_raw_as
 
 from corva.models.base import BaseEvent
-from corva.types import SCHEDULED_EVENT_TYPE, STREAM_EVENT_TYPE, TASK_EVENT_TYPE
-
-BaseEventTV = TypeVar('BaseEventTV', bound=BaseEvent)
-EventTypeTV = TypeVar('EventTypeTV', SCHEDULED_EVENT_TYPE, STREAM_EVENT_TYPE, TASK_EVENT_TYPE)
 
 
-class BaseLoader(Generic[BaseEventTV, EventTypeTV]):
-    def load(self, event: str) -> BaseEventTV:
-        return parse_raw_as(EventTypeTV, event)
+class BaseLoader(ABC):
+    parse_as_type: ClassVar[Any]
+
+    @abstractmethod
+    def load(self, event: str) -> BaseEvent:
+        pass
+
+    @classmethod
+    def parse(cls, event: str) -> Any:
+        return parse_raw_as(cls.parse_as_type, event)
