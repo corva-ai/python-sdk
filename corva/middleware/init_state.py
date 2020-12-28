ad511bde3389376749beb53a20857b89458649ab
Oleksii Symon <oleksii.symon@corva.ai>
2020-12-28 12:28:41 +0200

    added init_state.py middleware

--- corva/middleware/init_state.py
+++ corva/middleware/init_state.py
@@ -1,37 +1,37 @@
 from logging import Logger
-from typing import Any, Callable, Union
+from typing import Callable, Optional, Union
 
-from corva.logger import DEFAULT_LOGGER
-from corva.models.base import BaseContext
 from corva.models.scheduled import ScheduledContext
 from corva.models.stream import StreamContext
 from corva.state.redis_adapter import RedisAdapter
 from corva.state.redis_state import RedisState
 
 
-class InitStateMiddleware:
-    def __init__(
-         self,
-         call: Callable[[BaseContext], Any],
-         *,
-         default_name: str,
-         cache_url: str,
-         cache_kwargs: dict = None,
-         logger: Logger = DEFAULT_LOGGER
-    ):
-        self.call = call
-        self.default_name = default_name
-        self.cache_url = cache_url
-        self.cache_kwargs = cache_kwargs or {}
-        self.logger = logger
-
-    def __call__(self, context: Union[StreamContext, ScheduledContext]) -> Any:
-        adapter = RedisAdapter(
-            default_name=self.default_name,
-            cache_url=self.cache_url,
-            logger=self.logger,
-            **self.cache_kwargs
+def init_state_factory(
+     *,
+     default_name: str,
+     cache_url: str,
+     cache_kwargs: Optional[dict] = None,
+     logger: Optional[Logger] = None
+) -> Callable:
+    def init_state(
+         context: Union[StreamContext, ScheduledContext], call_next: Callable
+    ) -> Union[StreamContext, ScheduledContext]:
+        adapter_params = dict(
+            default_name=default_name,
+            cache_url=cache_url,
+            **cache_kwargs
         )
-        context.state = RedisState(redis=adapter, logger=self.logger)
+        state_params = {}
 
-        return self.call(context)
+        if logger is not None:
+            adapter_params['logger'] = logger
+            state_params['logger'] = logger
+
+        context.state = RedisState(redis=RedisAdapter(**adapter_params), **state_params)
+
+        context = call_next(context)
+
+        return context
+
+    return init_state
