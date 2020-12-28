6b03270bfabcba3bdf12699822df076c429d06f7
Oleksii Symon <oleksii.symon@corva.ai>
2020-12-28 12:32:24 +0200

    refactored types.py

--- corva/types.py
+++ corva/types.py
@@ -2,5 +2,5 @@ from typing import Any, Callable, TypeVar, Union
 
 _T = TypeVar('_T')
 REDIS_STORED_VALUE_TYPE = Union[bytes, str, int, float]
-MIDDLEWARE_TYPE = Callable[[_T, Callable[[_T], Any]], Any]
 MIDDLEWARE_CALL_TYPE = Callable[[_T], Any]
+MIDDLEWARE_TYPE = Callable[[_T, MIDDLEWARE_CALL_TYPE], Any]
