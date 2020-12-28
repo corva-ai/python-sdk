d3d86a8cf7bb507063d9ddb8182d324ae33565fa
Oleksii Symon <oleksii.symon@corva.ai>
2020-12-28 16:53:52 +0200

    added stream.py middleware

---
+++ corva/middleware/stream.py
@@ -0,0 +1,33 @@
+from itertools import chain
+from typing import Callable
+
+from corva.models.stream import StreamContext, StreamStateData
+
+
+def stream(context: StreamContext, call_next: Callable) -> StreamContext:
+    context = call_next(context)  # type: StreamContext
+
+    all_records = list(chain(*[subdata.records for subdata in context.event]))
+
+    last_processed_timestamp = max(
+        [
+            record.timestamp
+            for record in all_records
+            if record.timestamp is not None
+        ],
+        default=StreamStateData.__fields__['last_processed_timestamp'].default
+    )
+    last_processed_depth = max(
+        [
+            record.measured_depth
+            for record in all_records
+            if record.measured_depth is not None
+        ],
+        default=StreamStateData.__fields__['last_processed_depth'].default
+    )
+
+    context.state_data = StreamStateData(
+        last_processed_timestamp=last_processed_timestamp, last_processed_depth=last_processed_depth
+    )
+
+    return context
