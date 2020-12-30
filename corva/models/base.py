a04da5e3c5da1b4eb674fbccc32b204aaac9b1c7
Oleksii Symon <oleksii.symon@corva.ai>
2020-12-30 14:40:18 +0200

    moved event, api and state loading to properties of BaseContext

--- corva/models/base.py
+++ corva/models/base.py
@@ -3,10 +3,11 @@ from __future__ import annotations
 from abc import ABC, abstractmethod
 from typing import Any, Generic, List, Optional, TypeVar
 
-from pydantic import BaseModel, Extra
+from pydantic import BaseModel, Extra, PrivateAttr
 from pydantic.generics import GenericModel
 
 from corva.network.api import Api
+from corva.state.redis_adapter import RedisAdapter
 from corva.state.redis_state import RedisState
 
 
@@ -42,11 +43,78 @@ class BaseContext(GenericModel, Generic[BaseEventTV, BaseDataTV]):
     raw_event: str
     app_key: str
 
-    event: Optional[BaseEventTV] = None
-    api: Optional[Api] = None
-    state: Optional[RedisState] = None
+    _event: BaseEventTV = PrivateAttr()
+    _api: Api = PrivateAttr()
+    _state: RedisState = PrivateAttr()
     state_data: Optional[BaseDataTV] = None
     user_result: Any = None
+
+    # api params
+    api_url: str
+    api_data_url: str
+    api_key: str
+    api_name: str
+    api_timeout: Optional[int] = None
+    api_max_retries: Optional[int] = None
+
+    # cache params
+    cache_url: Optional[str] = None
+    cache_kwargs: dict = {}
+
+    @property
+    def provider(self) -> str:
+        return self.app_key.split('.')[0]
+
+    @property
+    def cache_key(self) -> str:
+        event = self.event
+
+        if isinstance(event, list):
+            event = event[0]
+
+        return (
+            f'{self.provider}/well/{event.asset_id}/stream/{event.app_stream_id}/'
+            f'{self.app_key}/{event.app_connection_id}'
+        )
+
+    @property
+    def event(self) -> BaseEventTV:
+        if self._event is None:
+            self._event = BaseEventTV.from_raw_event(self.raw_event, app_key=self.app_key)
+
+        return self._event
+
+    @property
+    def api(self) -> Api:
+        if self._api is None:
+            kwargs = dict(
+                api_url=self.api_url,
+                data_api_url=self.api_data_url,
+                api_key=self.api_key,
+                api_name=self.api_name
+            )
+
+            if self.api_timeout is not None:
+                kwargs['timeout'] = self.api_timeout
+            if self.api_timeout is not None:
+                kwargs['max_retries'] = self.api_max_retries
+
+            self._api = Api(**kwargs)
+
+        return self._api
+
+    @property
+    def state(self) -> RedisState:
+        if self._state is None:
+            adapter_params = dict(
+                default_name=self.cache_key,
+                cache_url=self.cache_url,
+                **self.cache_kwargs
+            )
+
+            self._state = RedisState(redis=RedisAdapter(**adapter_params))
+
+        return self._state
 
 
 class ListEvent(BaseEvent, List[BaseDataTV]):
