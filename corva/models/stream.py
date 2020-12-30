9e8f04693296df30011501aa984fead335f3fe6c
Oleksii Symon <oleksii.symon@corva.ai>
2020-12-30 11:31:45 +0200

    unnested classes in models/stream.py

--- corva/models/stream.py
+++ corva/models/stream.py
@@ -8,31 +8,35 @@ class StreamContext(BaseContext):
     state: RedisState
 
 
+class RecordData(BaseEventData):
+    hole_depth: Optional[float] = None
+    weight_on_bit: Optional[int] = None
+    state: Optional[str] = None
+
+
+class Record(BaseEventData):
+    timestamp: Optional[int] = None
+    asset_id: int
+    company_id: int
+    version: int
+    measured_depth: Optional[float] = None
+    collection: str
+    data: RecordData
+
+
+class AppMetadata(BaseEventData):
+    app_connection_id: int
+
+
+class StreamEventMetadata(BaseEventData):
+    app_stream_id: int
+    apps: Dict[str, AppMetadata]
+
+
 class StreamEventData(BaseEventData):
-    class Record(BaseEventData):
-        class Data(BaseEventData):
-            hole_depth: Optional[float] = None
-            weight_on_bit: Optional[int] = None
-            state: Optional[str] = None
-
-        timestamp: Optional[int] = None
-        asset_id: int
-        company_id: int
-        version: int
-        measured_depth: Optional[float] = None
-        collection: str
-        data: Data
-
-    class Metadata(BaseEventData):
-        class AppData(BaseEventData):
-            app_connection_id: int
-
-        app_stream_id: int
-        apps: Dict[str, AppData]
-
     app_key: Optional[str] = None
     records: List[Record]
-    metadata: Metadata
+    metadata: StreamEventMetadata
 
     @property
     def asset_id(self) -> int:
