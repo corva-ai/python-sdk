9322119d25dfe5ef1b727d9ab9e8a5bf08056c89
Oleksii Symon <oleksii.symon@corva.ai>
2020-12-30 12:57:32 +0200

    changed loader_factory to use new loaders

--- corva/middleware/loader.py
+++ corva/middleware/loader.py
@@ -1,13 +1,14 @@
-from typing import Callable
+from typing import Callable, Optional
 
-from corva.loader.base import BaseLoader
 from corva.models.base import BaseContext
 
 
-def loader_factory(loader: BaseLoader) -> Callable:
+def loader_factory(loader: Callable, loader_kwargs: Optional[dict] = None) -> Callable:
     def loader_(context: BaseContext, call_next: Callable) -> BaseContext:
-        context.event = loader.load(event=context.raw_event)
+        context.event = loader(context.raw_event, **(loader_kwargs or {}))
+
         context = call_next(context)
+
         return context
 
     return loader_
