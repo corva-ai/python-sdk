54a353dcaa48f06acadc450654e9ad7e843d2dfa
Oleksii Symon <oleksii.symon@corva.ai>
2020-12-30 11:10:12 +0200

    updated unpack_context.py

--- corva/middleware/unpack_context.py
+++ corva/middleware/unpack_context.py
@@ -1,82 +1,26 @@
-import inspect
-from typing import Callable, Optional, Tuple
+from typing import Callable
 
-from pydantic.utils import lenient_issubclass
-
-from corva.models.base import BaseContext, BaseEvent
-from corva.network.api import Api
-from corva.state.redis_state import RedisState
+from corva.models.base import BaseContext
 
 
-def _parse_call(call: Callable) -> Tuple[Optional[str], Optional[str], Optional[str], Optional[str]]:
-    """
-    Helper function, that looks for arguments with special type annotations
-    and returns names of those arguments
+def unpack_context_factory(include_state=False, include_context=False):
+    def unpack_context(context: BaseContext, call_next: Callable) -> BaseContext:
+        """
+        Calls user function with 'unpacked' arguments from context.
 
-    Example, when 'Event' and 'Api' are special type annotations:
+        Corva app passes some arguments to user's function by default (e.g event, api),
+        this middleware 'unpacks' arguments from context and calls user's function with them.
+        """
 
-        def foo(event: 'Event', api: 'Api'): pass
+        args = [context.event, context.api]
 
-        _parse_call(foo)  # returns ('event', 'api')
-    """
+        if include_state:
+            args.append(context.state)
+        if include_context:
+            args.append(context)
 
-    event_param_name = None
-    api_param_name = None
-    state_param_name = None
-    context_param_name = None
+        context.user_result = call_next(*args)
 
-    # iterate over each parameter in signature
-    for param in inspect.signature(call).parameters.values():
-        name = param.name
-        annotation = param.annotation
+        return context
 
-        # look for annotations with selected types and store argument names
-        if lenient_issubclass(annotation, BaseEvent):
-            event_param_name = name
-        elif lenient_issubclass(annotation, Api):
-            api_param_name = name
-        elif lenient_issubclass(annotation, RedisState):
-            state_param_name = name
-        elif lenient_issubclass(annotation, BaseContext):
-            context_param_name = name
-
-    return event_param_name, api_param_name, state_param_name, context_param_name
-
-
-def unpack_context(context: BaseContext, call_next: Callable) -> BaseContext:
-    """
-    Looks for arguments with special type annotations in function signature,
-    then injects values from context into those arguments and calls the function.
-
-    Example, when 'Event' and 'Api' are special type annotations:
-
-        def foo(event: 'Event', api: 'Api'): pass
-
-        @dataclass
-        class Context:
-            event: Event = None
-            api: Api = = None
-
-        context = Context()
-
-        # will `unpack` the context and call foo like this: foo(event=context.event, api=context.api)
-        unpack_context(context, foo)
-    """
-
-    event_param_name, api_param_name, state_param_name, context_param_name = _parse_call(call=call_next)
-
-    kwargs = context.user_kwargs.copy()
-
-    # populate kwargs with found argument names and values from context
-    if event_param_name:
-        kwargs[event_param_name] = context.event
-    if state_param_name:
-        kwargs[state_param_name] = context.state
-    if api_param_name:
-        kwargs[api_param_name] = context.api
-    if context_param_name:
-        kwargs[context_param_name] = context
-
-    context.user_result = call_next(**kwargs)
-
-    return context
+    return unpack_context
