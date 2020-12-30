8ce95520765f39ef7c016cfe1b41f930342565d3
Oleksii Symon <oleksii.symon@corva.ai>
2020-12-30 11:27:02 +0200

    deleted keyword only arguments in loader_factory as it has only one argument

--- corva/middleware/loader.py
+++ corva/middleware/loader.py
@@ -4,7 +4,7 @@ from corva.loader.base import BaseLoader
 from corva.models.base import BaseContext
 
 
-def loader_factory(*, loader: BaseLoader) -> Callable:
+def loader_factory(loader: BaseLoader) -> Callable:
     def loader_(context: BaseContext, call_next: Callable) -> BaseContext:
         context.event = loader.load(event=context.raw_event)
         context = call_next(context)
