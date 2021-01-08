81ebf238d042a82f38668dca6bf9353d5d971dcc
Oleksii Symon <oleksii.symon@corva.ai>
2021-01-08 14:36:32 +0200

    added Settings to corva.__init__.py

--- corva/__init__.py
+++ corva/__init__.py
@@ -2,3 +2,4 @@ from .application import Corva
 from .models.stream import StreamEvent
 from .network.api import Api
 from .state.redis_state import RedisState as State
+from .settings import Settings
