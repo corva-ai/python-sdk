7dc43855e9f8afec3e2a7d64ff3b4afa836b2954
Oleksii Symon <oleksii.symon@corva.ai>
2021-01-08 14:38:53 +0200

    updated tutorial_2_configuration.py

--- docs_src/tutorial_2_configuration.py
+++ docs_src/tutorial_2_configuration.py
@@ -1,19 +1,9 @@
 from corva import Api, Corva, StreamEvent, State
 
-app = Corva(
-    # 1 api params
-    api_url='api.localhost',
-    api_data_url='api.data.localhost',
-    api_key='api_key',
-    api_app_name='api_app_name',
-
-    # 2 state params
-    state_url='redis://',
-    state_params={'param1': 'val1'}
-)
+app = Corva()
 
 
-@app.stream
+@app.stream(filter_by_timestamp=True)
 def stream_app(event: StreamEvent, api: Api, state: State):
     """User's main logic function"""
 
