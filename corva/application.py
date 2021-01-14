61f9d1849c732d561e0129651d8eec0d5dda5317
Oleksii Symon <oleksii.symon@corva.ai>
2021-01-14 18:13:36 +0200

    added settings and stream to Corva

--- corva/application.py
+++ corva/application.py
@@ -1,63 +1,72 @@
+from typing import Any, Callable, List, Optional
+
+from corva.models.stream import StreamContext, StreamEvent
+from corva.network.api import Api
+from corva.settings import Settings
+from corva.stream import stream
+
+
 class Corva:
+    def __init__(
+         self,
+         api_url: Optional[str] = None,
+         data_api_url: Optional[str] = None,
+         cache_url: Optional[str] = None,
+         api_key: Optional[str] = None,
+         app_key: Optional[str] = None,
+         api_timeout: Optional[int] = None,
+         api_max_retries: Optional[int] = None,
+         cache_kwargs: Optional[dict] = None
+    ):
+        self.settings = Settings()
+        self.cache_kwargs = cache_kwargs
+
+        if api_url is not None:
+            self.settings.API_ROOT_URL = api_url
+        if data_api_url is not None:
+            self.settings.DATA_API_ROOT_URL = data_api_url
+        if cache_url is not None:
+            self.settings.CACHE_URL = cache_url
+        if api_key is not None:
+            self.settings.API_KEY = api_key
+        if app_key is not None:
+            self.settings.APP_KEY = app_key
+
+        api_kwargs = {}
+        if api_timeout is not None:
+            api_kwargs['timeout'] = api_timeout
+        if api_max_retries is not None:
+            api_kwargs['max_retries'] = api_max_retries
+        self.api = Api(
+            api_url=self.settings.API_ROOT_URL,
+            data_api_url=self.settings.DATA_API_ROOT_URL,
+            api_key=self.settings.API_KEY,
+            app_name=self.settings.APP_NAME,
+            **api_kwargs
+        )
 
     def stream(
          self,
-         func=None,
+         fn: Callable,
+         event: str,
          *,
          filter_by_timestamp: bool = False,
-         filter_by_depth: bool = False,
+         filter_by_depth: bool = False
+    ) -> List[Any]:
+        events = StreamEvent.from_raw_event(event=event, app_key=self.settings.APP_KEY)
 
-         settings: Optional[Settings] = None,
+        results = []
 
-         # api params
-         api_timeout: Optional[int] = None,
-         api_max_retries: Optional[int] = None,
-
-         # cache params
-         cache_kwargs: Optional[dict] = None
-    ) -> Callable:
-        """Decorates a function to be a stream one
-
-        Can be used both with and without arguments.
-        https://github.com/dabeaz/python-cookbook/blob/master/src/9/defining_a_decorator_that_takes_an_optional_argument/example.py
-        """
-
-        if func is None:
-            return partial(
-                self.stream,
+        for event in events:
+            ctx = StreamContext(
+                event=event,
+                settings=self.settings,
+                api=self.api,
+                cache_kwargs=self.cache_kwargs,
                 filter_by_timestamp=filter_by_timestamp,
-                filter_by_depth=filter_by_depth,
-                settings=settings,
-                api_timeout=api_timeout,
-                api_max_retries=api_max_retries,
-                cache_kwargs=cache_kwargs
+                filter_by_depth=filter_by_depth
             )
 
-        @wraps(func)
-        def wrapper(event) -> List[Any]:
-            settings_ = settings or SETTINGS.copy()
+            results.append(stream(fn)(context=ctx))
 
-            middleware = [stream] + self.user_middleware + [unpack_context_factory(include_state=True)]
-
-            call = wrap_call_in_middleware(call=func, middleware=middleware)
-
-            events = StreamEvent.from_raw_event(event=event, app_key=settings_.APP_KEY)
-
-            results = []
-
-            for event in events:
-                ctx = StreamContext(
-                    event=event,
-                    settings=settings_,
-                    api_timeout=api_timeout,
-                    api_max_retries=api_max_retries,
-                    cache_kwargs=cache_kwargs,
-                    filter_by_timestamp=filter_by_timestamp,
-                    filter_by_depth=filter_by_depth
-                )
-                ctx = call(ctx)  # type: StreamContext
-                results.append(ctx.user_result)
-
-            return results
-
-        return wrapper
+        return results
