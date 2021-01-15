ffbdd8841f395af226f67556239130a332792ced
Oleksii Symon <oleksii.symon@corva.ai>
2021-01-15 15:36:00 +0200

    improved Api type hints

--- tests/test_scheduled_app.py
+++ tests/test_scheduled_app.py
@@ -1,15 +1,15 @@
-from unittest.mock import Mock, MagicMock
+from unittest.mock import Mock, MagicMock, PropertyMock
 
 from corva.application import Corva
 
 
 def scheduled_app(event, api, state):
     api.session.request = MagicMock()
-    api.post = Mock(wraps=api.post)
+    type(api).post = PropertyMock(return_value=Mock(wraps=api.post))
     return api
 
 
-def test_set_completed_status():
+def test_set_completed_status(mocker):
     event = (
         '[[{"cron_string": "", "environment": "", "app": 0, "app_key": "", "app_connection_id": 0, "app_stream_id": 0, '
         '"source_type": "", "company": 0, "provider": "", "schedule": 0, "interval": 0, '
