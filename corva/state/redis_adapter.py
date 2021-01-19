5596373febb5e0e22844b1bf15c6c8a42e745608
Oleksii Symon <oleksii.symon@corva.ai>
2021-01-19 15:17:56 +0200

    added docs to RedisAdapter

--- corva/state/redis_adapter.py
+++ corva/state/redis_adapter.py
@@ -42,6 +42,18 @@ class RedisAdapter(Redis):
          mapping: Optional[Dict[str, REDIS_STORED_VALUE_TYPE]] = None,
          expiry: Union[int, timedelta, None] = DEFAULT_EXPIRY
     ) -> int:
+        """Stores the data in cache
+
+        params:
+         key: key, which will contain the data
+         value: data to be saved
+         mapping: dict of key:data pairs to be saved
+         expiry: time in seconds for when data will be deleted from cache
+            expiration is reset with every hset call
+            expiration can be disabled by setting expiry to None
+        returns: number of inserted elements
+        """
+
         name = name or self.default_name
 
         n_set = super().hset(name=name, key=key, value=value, mapping=mapping)
@@ -55,29 +67,53 @@ class RedisAdapter(Redis):
         return n_set
 
     def hget(self, key: str, name: Optional[str] = None) -> Union[REDIS_STORED_VALUE_TYPE, None]:
+        """Loads data from cache
+
+        params:
+         key: key to load data from
+        returns: stored data
+        """
+
         name = name or self.default_name
         return super().hget(name=name, key=key)
 
     def hgetall(self, name: Optional[str] = None) -> Dict[str, Union[REDIS_STORED_VALUE_TYPE]]:
+        """Loads all data from cache"""
+
         name = name or self.default_name
         return super().hgetall(name=name)
 
     def hdel(self, keys: List[str], name: Optional[str] = None) -> int:
+        """Deletes some data from cache
+
+        params:
+         keys: list of keys to delete
+        returns: number of deleted elements
+        """
+
         name = name or self.default_name
         return super().hdel(name, *keys)
 
     def delete(self, *names: List[str]) -> int:
+        """Deletes all data from cache"""
+
         names = names or [self.default_name]
         return super().delete(*names)
 
     def ttl(self, name: Optional[str] = None) -> int:
+        """Returns the number of seconds until expiration"""
+
         name = name or self.default_name
         return super().ttl(name=name)
 
     def pttl(self, name: Optional[str] = None) -> int:
+        """Returns the number of milliseconds until expiration"""
+
         name = name or self.default_name
         return super().pttl(name=name)
 
     def exists(self, *names: List[str]) -> int:
+        """Returns whether there is data in cache"""
+
         names = names or [self.default_name]
         return super().exists(*names)
