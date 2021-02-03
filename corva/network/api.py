16e1898fb10c0b702f5d18e62196c6d04b476680
Oleksii Symon <oleksii.symon@corva.ai>
2021-02-03 18:37:30 +0200

    added new Api class

--- corva/network/api.py
+++ corva/network/api.py
@@ -1,134 +1,111 @@
-import os
+import posixpath
 import re
-from typing import List, Optional
+from typing import Optional
 
-from requests import Response, Session
-from requests.adapters import HTTPAdapter
-from urllib3 import Retry
+import requests
+import requests.adapters
+import urllib3
 
 
-class Api:
+class Api(requests.Session):
     """Provides a convenient way to access Corva API and Corva Data API
 
-    Api is a thin wrapper around `requests` library that handles
-     authorization, adds timeouts and retries to request.
+    Api is a thin wrapper around `requests.Session` that adds
+     authorization, timeouts and retries to requests.
     """
 
-    ALLOWED_METHODS = {'GET', 'POST', 'PATCH', 'PUT', 'DELETE'}
-    DEFAULT_TIMEOUT = 600
-    DEFAULT_MAX_RETRIES = 3
+    MAX_RETRIES = 3
+    TIMEOUT = 30  # seconds
 
     def __init__(
-         self,
-         api_url: str,
-         data_api_url: str,
-         api_key: str,
-         app_name: str,
-         timeout: Optional[int] = None,
-         max_retries: Optional[int] = None
+        self,
+        api_url: str,
+        data_api_url: str,
+        api_key: str,
+        app_name: str,
+        timeout: Optional[int] = None,
+        max_retries: Optional[int] = None,
     ):
-        self.timeout = timeout or self.DEFAULT_TIMEOUT
-        self.max_retries = max_retries or self.DEFAULT_MAX_RETRIES
+        super().__init__()
+
+        self.timeout = timeout or self.TIMEOUT
+        self.max_retries = max_retries or self.MAX_RETRIES
         self.api_url = api_url
         self.data_api_url = data_api_url
         self.api_key = api_key
         self.app_name = app_name
-        self.session = self._init_session(
-            api_key=api_key,
-            app_name=app_name,
-            max_retries=self.max_retries,
-            allowed_methods=list(self.ALLOWED_METHODS)
+
+        self.headers.update(
+            {
+                'Authorization': f'API {api_key}',
+                'X-Corva-App': app_name,
+            }
         )
 
-    @staticmethod
-    def _init_session(api_key: str, app_name: str, max_retries: int, allowed_methods: List[str]):
-        session = Session()
-
-        session.headers.update({
-            'Authorization': f'API {api_key}',
-            'X-Corva-App': app_name
-        })
-        session.mount(
+        self.mount(
             'https://',
-            HTTPAdapter(
-                max_retries=Retry(
-                    total=max_retries,
+            requests.adapters.HTTPAdapter(
+                max_retries=urllib3.Retry(
+                    total=self.max_retries,
                     status_forcelist=[408, 429, 500, 502, 503, 504],
-                    allowed_methods=allowed_methods,
                     backoff_factor=0.3,
-                    raise_on_status=False
+                    raise_on_redirect=False,
+                    raise_on_status=False,
                 )
-            )
+            ),
         )
 
-        return session
+    def _get_url(self, suffix: str) -> str:
+        """Builds complete url from base prefix and suffix
 
-    def get(self, path: str, **kwargs):
-        return self._request('GET', path, **kwargs)
+        returns:
+         1 suffix param, if suffix is a complete url
+         2 data api url, if suffix contains data api url pattern
+         3 corva api url, if above points are not True
+        """
 
-    def post(self, path: str, **kwargs):
-        return self._request('POST', path, **kwargs)
+        if suffix.startswith('http'):
+            return suffix
 
-    def patch(self, path: str, **kwargs):
-        return self._request('PATCH', path, **kwargs)
+        suffix = suffix.lstrip(
+            '/'
+        )  # delete leading forward slash for posixpath.join to work correctly
 
-    def put(self, path: str, **kwargs):
-        return self._request('PUT', path, **kwargs)
+        # search text like api/v1 or api/v10 in path
+        if bool(re.search(r'api/v\d+', suffix)):
+            return posixpath.join(self.data_api_url, suffix)
 
-    def delete(self, path: str, **kwargs):
-        return self._request('DELETE', path, **kwargs)
+        return posixpath.join(self.api_url, suffix)
 
-    def _get_url(self, path: str):
-        # search text like api/v1/data or api/v1/message_producer in path
-        if bool(re.search(r'api/v\d+/(data|message_producer)', path)):
-            base_url = self.data_api_url
-        else:
-            base_url = self.api_url
-
-        return os.path.join(base_url.strip('/'), path.strip('/'))
-
-    def _request(
-         self,
-         method: str,
-         path: str,
-         data: Optional[dict] = None,
-         params: Optional[dict] = None,
-         headers: Optional[dict] = None,
-         max_retries: Optional[int] = None,
-         timeout: Optional[int] = None
-    ) -> Response:
+    def request(
+        self,
+        method: str,
+        url: str,
+        data: Optional[dict] = None,
+        max_retries: Optional[int] = None,
+        **kwargs,
+    ) -> requests.Response:
         """Executes the request
 
         params:
          method: HTTP method
-         path: url to call
-         data: request body
-         params: url query string params
-         headers: additional headers to include in request
+         url: url to call
+         data: request body (will be automatically casted to json)
          max_retries: custom value for max number of retries
-         timeout: request timeout in seconds
-        returns: response
-        raises: HTTPError for unsuccessful requests
+        raises:
+         requests.HTTPError for unsuccessful request without retries
+         urllib3.exceptions.MaxRetryError for unsuccessful request with retries
         """
 
-        if method not in self.ALLOWED_METHODS:
-            raise ValueError(f'Invalid HTTP method {method}.')
-
         max_retries = max_retries or self.max_retries
-        timeout = timeout or self.timeout
+        kwargs.setdefault('timeout', self.timeout)
+
+        if data is not None:  # Corva endpoints work only with json data
+            kwargs['json'] = data
 
         # not thread safe
-        self.session.adapters['https://'].max_retries.total = max_retries
+        self.adapters['https://'].max_retries.total = max_retries
 
-        response = self.session.request(
-            method=method,
-            url=self._get_url(path=path),
-            params=params,
-            json=data,
-            headers=headers,
-            timeout=timeout
-        )
-
-        response.raise_for_status()
+        response = super().request(method, self._get_url(url), **kwargs)
 
         return response
