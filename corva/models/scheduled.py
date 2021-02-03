2df5af6d632886ed6c026efa8a489065b3ce82c3
Oleksii Symon <oleksii.symon@corva.ai>
2021-02-03 19:35:56 +0200

    deleted non-essential fields from ScheduledEvent

--- corva/models/scheduled.py
+++ corva/models/scheduled.py
@@ -2,42 +2,22 @@ from __future__ import annotations
 
 import itertools
 from datetime import datetime
-from typing import List, Optional
+from typing import List
 
 import pydantic
 
-from corva.models.base import BaseContext, BaseEvent, CorvaBaseModel
+from corva.models.base import BaseContext, BaseEvent
 
 
-class ScheduledEventData(CorvaBaseModel):
-    type: Optional[str] = None
-    collection: Optional[str] = None
-    cron_string: str
-    environment: str
-    app: int
-    app_key: str
-    app_version: Optional[int]
-    app_connection_id: int = pydantic.Field(alias='app_connection')
-    app_stream_id: int = pydantic.Field(alias='app_stream')
-    source_type: str
-    company: int
-    provider: str
-    api_url: Optional[str] = None
-    api_key: Optional[str] = None
-    schedule: int
-    interval: int
+class ScheduledEvent(BaseEvent):
+    asset_id: int
+    interval: int = pydantic.Field(
+        ..., description='Scheduled interval (parsed cron string in seconds)'
+    )
+    schedule_id: int = pydantic.Field(..., alias='schedule')
     schedule_start: datetime
     schedule_end: datetime
-    asset_id: int
-    asset_name: str
-    asset_type: str
-    timezone: str
-    log_type: str
-    log_identifier: Optional[str] = None
-    day_shift_start: Optional[str] = None
 
-
-class ScheduledEvent(BaseEvent, ScheduledEventData):
     @staticmethod
     def from_raw_event(event: str, **kwargs) -> List[ScheduledEvent]:
         events = pydantic.parse_raw_as(List[List[ScheduledEvent]], event)
