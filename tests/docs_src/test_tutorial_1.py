d7f75e1cb7c0fbd80ead77957c7fe36e16fb9f5f
Oleksii Symon <oleksii.symon@corva.ai>
2021-02-05 15:27:17 +0200

    deleted stream AppMetadata.app_version field

--- tests/docs_src/test_tutorial_1.py
+++ tests/docs_src/test_tutorial_1.py
@@ -6,7 +6,7 @@ from docs_src import tutorial_1_hello_world
 def test_tutorial(settings):
     event = (
                 '[{"records": [{"asset_id": 0, "company_id": 0, "version": 0, "collection": "", "data": {}}], '
-                '"metadata": {"app_stream_id": 0, "apps": {"%s": {"app_connection_id": 0, "app_version": 0}}}}]'
+                '"metadata": {"app_stream_id": 0, "apps": {"%s": {"app_connection_id": 0}}}}]'
             ) % settings.APP_KEY
     context = SimpleNamespace(client_context=None)
 
