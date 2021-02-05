8893009db43743df311eccd3416279338b6b8e29
Oleksii Symon <oleksii.symon@corva.ai>
2021-02-05 13:45:32 +0200

    Fixed and added new tests to test_api.py

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,26 +1,100 @@
+from typing import Literal
+
 import pytest
+from pytest_mock import MockerFixture
+
+from corva.application import Corva
+from corva.configuration import SETTINGS
 
 
-def test_default_headers(api):
-    assert not {'Authorization', 'X-Corva-App'} - set(api.session.headers)
+@pytest.fixture(scope='function')
+def event():
+    return (
+        '[{"records": [{"asset_id": 0, "company_id": 0, "version": 0, "collection": "", "data": {}}], '
+        '"metadata": {"app_stream_id": 0, "apps": {"%s": {"app_connection_id": 0, "app_version": 0}}}}]'
+    ) % SETTINGS.APP_KEY
 
 
-def test_get_url(api):
-    path = '/api/v10/data'
-    assert api._get_url(path=path) == f'{api.data_api_url}{path}'
-
-    path = 'api/v10/data'
-    assert api._get_url(path=path) == f'{api.data_api_url}/{path}'
-
-    path = 'api/v10/message_producer'
-    assert api._get_url(path=path) == f'{api.data_api_url}/{path}'
-
-    path = 'api/v10/path'
-    assert api._get_url(path=path) == f'{api.api_url}/{path}'
+def app(event, api, cache):
+    return api
 
 
-def test_request_invalid_method(api):
-    method = 'random'
-    with pytest.raises(ValueError) as exc:
-        api._request(method=method, path='random')
-    assert str(exc.value) == f'Invalid HTTP method {method}.'
+def test_default_headers(event, mocker: MockerFixture):
+    request_patch = mocker.patch('requests.request')
+
+    api = Corva(context=None).stream(app, event)[0]
+
+    api.get('')  # do some api call
+
+    request_patch.assert_called_once()
+    assert 'Authorization' in request_patch.call_args.kwargs['headers']
+    assert 'X-Corva-App' in request_patch.call_args.kwargs['headers']
+
+
+@pytest.mark.parametrize(
+    'path,url,type_',
+    [
+        ['http://localhost', 'http://localhost', ''],
+        ['api/v10/path', '%s/api/v10/path', 'data'],
+        ['/api/v10/path', '%s/api/v10/path', 'data'],
+        ['v10/path', '%s/v10/path', 'corva'],
+        ['/v10/path', '%s/v10/path', 'corva'],
+    ],
+)
+def test_get_url(
+    event, mocker: MockerFixture, path, url, type_: Literal['data', 'corva']
+):
+    request_patch = mocker.patch('requests.request')
+
+    api = Corva(context=None).stream(app, event)[0]
+
+    api.get(path)
+
+    expected = url
+
+    if type_ == 'data':
+        expected = url % SETTINGS.DATA_API_ROOT_URL
+
+    if type_ == 'corva':
+        expected = url % SETTINGS.API_ROOT_URL
+
+    request_patch.assert_called_once()
+    assert request_patch.call_args.kwargs['url'] == expected
+
+
+def test_data_passed_as_json(event, mocker: MockerFixture):
+    request_patch = mocker.patch('requests.request')
+
+    api = Corva(context=None).stream(app, event)[0]
+
+    api.post('', data={})
+
+    request_patch.assert_called_once()
+    assert request_patch.call_args.kwargs['json'] == {}
+
+
+@pytest.mark.parametrize('timeout', [None, 12345])
+def test_timeout(event, mocker: MockerFixture, timeout):
+    request_patch = mocker.patch('requests.request')
+
+    api = Corva(context=None).stream(app, event)[0]
+
+    api.post('', timeout=timeout)
+    request_patch.assert_called_once()
+
+    assert timeout != api.timeout
+
+    expected = timeout or api.timeout
+
+    assert request_patch.call_args.kwargs['timeout'] == expected
+
+
+def test_custom_headers(event, mocker: MockerFixture):
+    request_patch = mocker.patch('requests.request')
+
+    api = Corva(context=None).stream(app, event)[0]
+
+    api.post('', headers={'custom': 'value'})
+
+    request_patch.assert_called_once()
+    assert request_patch.call_args.kwargs['headers']['custom'] == 'value'
