229743a16e71239b40adb2180993b639a814370a
Oleksii Symon <oleksii.symon@corva.ai>
2021-02-10 18:19:50 +0200

    deleted tests.conftest.settings fixture (to enforce usage of global SETTINGS)

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,7 @@ import pytest
 from fakeredis import FakeRedis, FakeServer
 
 from corva.api import Api
-from corva.configuration import Settings
+from corva.configuration import SETTINGS
 from corva.state.redis_adapter import RedisAdapter
 from corva.state.redis_state import RedisState
 
@@ -20,20 +20,26 @@ def patch_redis_adapter():
 
     redis_adapter_path = 'corva.state.redis_adapter'
 
-    redis_adapter_patcher = patch(f'{redis_adapter_path}.RedisAdapter.__bases__', (FakeRedis,))
+    redis_adapter_patcher = patch(
+        f'{redis_adapter_path}.RedisAdapter.__bases__', (FakeRedis,)
+    )
 
-    server = FakeServer()  # use FakeServer to share cache between different instances of RedisState
+    server = (
+        FakeServer()
+    )  # use FakeServer to share cache between different instances of RedisState
 
-    with redis_adapter_patcher, \
-         patch(f'{redis_adapter_path}.from_url', side_effect=partial(FakeRedis.from_url, server=server)):
+    with redis_adapter_patcher, patch(
+        f'{redis_adapter_path}.from_url',
+        side_effect=partial(FakeRedis.from_url, server=server),
+    ):
         # necessary to stop mock.patch from trying to call delattr when reversing the patch
         redis_adapter_patcher.is_local = True
         yield
 
 
 @pytest.fixture(scope='function')
-def redis_adapter(patch_redis_adapter, settings):
-    return RedisAdapter(default_name='default_name', cache_url=settings.CACHE_URL)
+def redis_adapter(patch_redis_adapter, patch_settings):
+    return RedisAdapter(default_name='default_name', cache_url=SETTINGS.CACHE_URL)
 
 
 @pytest.fixture(scope='function')
@@ -41,39 +47,32 @@ def redis(redis_adapter):
     return RedisState(redis=redis_adapter)
 
 
+# TODO: delete after getting rid of corva/app/base.py
 @pytest.fixture(scope='function')
 def api():
     return Api(
         api_url='https://api.localhost.ai',
         data_api_url='https://data.localhost.ai',
         api_key='',
-        app_name=''
-    )
-
-
-# TODO: delete fixture below and instead use mocked global settings from corva.configuration.SETTINGS
-@pytest.fixture(scope='function')
-def settings():
-    """proper corva settings for testing"""
-
-    return Settings(
-        APP_KEY='provider.app-name',
-        CACHE_URL='redis://localhost:6379',
-        API_ROOT_URL='https://api.localhost.ai',
-        DATA_API_ROOT_URL='https://data.localhost.ai',
-        API_KEY='123'
+        app_name='',
     )
 
 
 @pytest.fixture(scope='function', autouse=True)
-def patch_settings(settings, mocker):
-    """replaces empty values in global corva settings with proper test values"""
+def patch_settings(mocker):
+    """replaces values in global corva settings with proper test values"""
 
     settings_path = 'corva.configuration.SETTINGS'
 
     mocker.patch.multiple(
         settings_path,
-        **settings.dict()
+        **{
+            'APP_KEY': 'provider.app-name',
+            'CACHE_URL': 'redis://localhost:6379',
+            'API_ROOT_URL': 'https://api.localhost.ai',
+            'DATA_API_ROOT_URL': 'https://data.localhost.ai',
+            'API_KEY': '123',
+        },
     )
     yield
 
