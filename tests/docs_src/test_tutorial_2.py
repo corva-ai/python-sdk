8b5c19ef161fc3d4fee66d828f07d5df21b05d1e
Oleksii Symon <oleksii.symon@corva.ai>
2021-02-10 18:23:18 +0200

    fixed test_tutorial_2.py test

--- tests/docs_src/test_tutorial_2.py
+++ tests/docs_src/test_tutorial_2.py
@@ -5,10 +5,16 @@ from docs_src import tutorial_2_configuration
 
 
 def test_tutorial():
-    event = (
-        '[{"records": [{"asset_id": 0, "timestamp": 0}], '
-        '"metadata": {"app_stream_id": 0, "apps": {"%s": {"app_connection_id": 0}}}}]'
-    ) % SETTINGS.APP_KEY
+    event = [
+        {
+            "records": [{"asset_id": 0, "timestamp": 0}],
+            "metadata": {
+                "app_stream_id": 0,
+                "apps": {SETTINGS.APP_KEY: {"app_connection_id": 0}},
+            },
+        }
+    ]
+
     context = SimpleNamespace(client_context=None)
 
     tutorial_2_configuration.lambda_handler(event, context)
