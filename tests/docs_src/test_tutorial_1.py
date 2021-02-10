aa20edc9508cdc637e6baa69426d900a505186e3
Oleksii Symon <oleksii.symon@corva.ai>
2021-02-10 12:59:21 +0200

    use corva.configuration.SETTINGS in test_tutorial_1.py

--- tests/docs_src/test_tutorial_1.py
+++ tests/docs_src/test_tutorial_1.py
@@ -1,13 +1,14 @@
 from types import SimpleNamespace
 
+from corva.configuration import SETTINGS
 from docs_src import tutorial_1_hello_world
 
 
-def test_tutorial(settings):
+def test_tutorial():
     event = (
         '[{"records": [{"asset_id": 0, "timestamp": 0}], '
         '"metadata": {"app_stream_id": 0, "apps": {"%s": {"app_connection_id": 0}}}}]'
-    ) % settings.APP_KEY
+    ) % SETTINGS.APP_KEY
     context = SimpleNamespace(client_context=None)
 
     tutorial_1_hello_world.lambda_handler(event, context)
