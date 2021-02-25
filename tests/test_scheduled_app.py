205f5b39672fc94dae1d1c1cbb44f2a75d6fd64d
Oleksii Symon <oleksii.symon@corva.ai>
2021-02-25 13:35:34 +0200

    fixed test_scheduled_app.py

--- tests/test_scheduled_app.py
+++ tests/test_scheduled_app.py
@@ -1,18 +1,15 @@
-from types import SimpleNamespace
-from unittest.mock import Mock, patch
+from unittest.mock import Mock
 
 from corva.application import Corva
 
 
 def scheduled_app(event, api, state):
-    patch('requests.request').start()
-
     api.post = Mock(wraps=api.post)  # spy on api.post
 
     return api
 
 
-def test_set_completed_status():
+def test_set_completed_status(corva_context):
     event = [
         [
             {
@@ -27,7 +24,7 @@ def test_set_completed_status():
         ]
     ]
 
-    corva = Corva(SimpleNamespace(client_context=None))
+    corva = Corva(corva_context)
 
     results = corva.scheduled(scheduled_app, event)
 
