a2f355ed94cf80832a92a87074f4f71d65fb05fe
Oleksii Symon <oleksii.symon@corva.ai>
2021-02-25 13:29:16 +0200

    fixed Corva class to get api key only from context

--- corva/application.py
+++ corva/application.py
@@ -23,15 +23,9 @@ class Corva:
          cache_settings: additional cache settings
         """
 
-        # Default to the API Key configured as a the API_KEY environment variable
-        api_key = SETTINGS.API_KEY
         try:
-            # When available, use the API key from the client context
             api_key = context.client_context.env["API_KEY"]
         except (AttributeError, KeyError):
-            pass
-
-        if api_key is None:
             raise Exception('No API Key found.')
 
         self.cache_settings = cache_settings or {}
