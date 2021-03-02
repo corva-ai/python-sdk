f826afe0337b2b30529ae4e7da67a474e50ead6f
Oleksii Symon <oleksii.symon@corva.ai>
2021-03-02 10:45:00 +0200

    deleted allow_population_by_field_name from pydantic config

--- corva/models/base.py
+++ corva/models/base.py
@@ -12,7 +12,6 @@ from corva.state.redis_state import RedisState
 
 
 class CorvaModelConfig:
-    allow_population_by_field_name = True
     arbitrary_types_allowed = True
     extra = pydantic.Extra.allow
     validate_assignment = True
