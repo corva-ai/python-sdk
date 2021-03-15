dbe96745925fcfae43145292bcc32cd5853b4d71
Oleksii Symon <oleksii.symon@corva.ai>
2021-03-15 12:12:37 +0200

    moved Api._set_schedule_as_completed to runners.scheduled.set_schedule_as_completed

--- src/corva/runners/scheduled.py
+++ src/corva/runners/scheduled.py
@@ -1,12 +1,19 @@
 from typing import Any, Callable
 
+from corva.api import Api
 from corva.models.scheduled import ScheduledContext
+
+
+def set_schedule_as_completed(api: Api, schedule_id: int) -> None:
+    """Sets schedule as completed. Should never raise."""
+
+    api.post(path=f'scheduler/{schedule_id}/completed')
 
 
 def scheduled_runner(fn: Callable, context: ScheduledContext) -> Any:
     result = fn(context.event, context.api, context.cache)
 
     # should never raise
-    context.api._set_schedule_as_completed(schedule_id=context.event.schedule_id)
+    set_schedule_as_completed(api=context.api, schedule_id=context.event.schedule_id)
 
     return result
