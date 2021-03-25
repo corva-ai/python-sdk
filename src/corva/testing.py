89dabda8e987e30bc181f8de09b716dde6294b46
Oleksii Symon <oleksii.symon@corva.ai>
2021-03-25 12:42:48 +0200

    Added test_to_raw_task_event_is_up_to_date

--- src/corva/testing.py
+++ src/corva/testing.py
@@ -25,19 +25,23 @@ class TestClient:
         if isinstance(event, TaskEvent):
             return TestClient._run_task(fn=fn, event=event, context=TestClient._context)
 
-    @staticmethod
+    @classmethod
     def _run_task(
-        fn: Callable, event: TaskEvent, context: types.SimpleNamespace
+        cls, fn: Callable, event: TaskEvent, context: types.SimpleNamespace
     ) -> Any:
         patches = [
             mock.patch('corva.runners.task.get_task_event', return_value=event),
             mock.patch('corva.runners.task.update_task_data'),
         ]
 
-        raw_event = RawTaskEvent(task_id=str(), version=2).dict()
+        raw_event = cls._to_raw_task_event(event=event)
 
         try:
             [patch.start() for patch in patches]
             return fn(raw_event, context)
         finally:
             mock.patch.stopall()
+
+    @staticmethod
+    def _to_raw_task_event(event: TaskEvent):
+        return RawTaskEvent(task_id=str(), version=2).dict()
