13ed31977258357df36e1cd2b679922201e4014f
Oleksii Symon <oleksii.symon@corva.ai>
2021-03-30 14:30:09 +0300

    deleted unused functionality from conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,5 @@
 import pytest
 
-from corva.api import Api
 from corva.configuration import SETTINGS
 from corva.state.redis_adapter import RedisAdapter
 from corva.state.redis_state import RedisState
@@ -20,19 +19,3 @@ def redis(redis_adapter):
 @pytest.fixture(scope='function')
 def context():
     return TestClient._context
-
-
-# TODO: delete after getting rid of corva/app/base.py
-@pytest.fixture(scope='function')
-def api():
-    return Api(
-        api_url='https://api.localhost.ai',
-        data_api_url='https://data.localhost.ai',
-        api_key='',
-        app_name='',
-    )
-
-
-class ComparableException(Exception):
-    def __eq__(self, other):
-        return type(self) is type(other) and self.args == other.args
