59f6389da3f8e4888dc211a5073299c6517e9b7d
Oleksii Symon <oleksii.symon@corva.ai>
2021-03-30 12:42:15 +0300

    deleted tutorial files

--- tests/docs_src/test_tutorial_2.py
+++
@@ -1,7 +0,0 @@
-from docs_src import tutorial_2_configuration
-
-
-def test_tutorial(corva_context):
-    event = [{"records": [{"asset_id": 0, "timestamp": 0}]}]
-
-    tutorial_2_configuration.lambda_handler(event, corva_context)
