e077112ba4a94604fadafe84aef333f8338871a3
Oleksii Symon <oleksii.symon@corva.ai>
2021-03-30 11:32:22 +0300

    added TestClient._run_stream

--- src/corva/testing.py
+++ src/corva/testing.py
@@ -2,8 +2,21 @@ import types
 from typing import Any, Callable, ClassVar, Union
 from unittest import mock
 
+from corva.configuration import SETTINGS
 from corva.models.scheduled import RawScheduledEvent, ScheduledEvent
-from corva.models.stream import StreamEvent
+from corva.models.stream import (
+    LogType,
+    RawAppMetadata,
+    RawDepthRecord,
+    RawMetadata,
+    RawStreamDepthEvent,
+    RawStreamEvent,
+    RawStreamTimeEvent,
+    RawTimeRecord,
+    StreamDepthEvent,
+    StreamEvent,
+    StreamTimeEvent,
+)
 from corva.models.task import RawTaskEvent, TaskEvent
 
 
@@ -23,11 +36,17 @@ class TestClient:
         fn: Callable[[dict, Any], Any],
         event: Union[TaskEvent, StreamEvent, ScheduledEvent],
     ) -> Any:
+
         if isinstance(event, TaskEvent):
             return TestClient._run_task(fn=fn, event=event, context=TestClient._context)
 
         if isinstance(event, ScheduledEvent):
             return TestClient._run_scheduled(
+                fn=fn, event=event, context=TestClient._context
+            )
+
+        if isinstance(event, StreamEvent):
+            return TestClient._run_stream(
                 fn=fn, event=event, context=TestClient._context
             )
 
@@ -71,6 +90,74 @@ class TestClient:
                 )
             ]
         ]
+
+        try:
+            [patch.start() for patch in patches]
+            return fn(raw_event, context)[0]
+        finally:
+            mock.patch.stopall()
+
+    @staticmethod
+    def _run_stream(
+        fn: Callable,
+        event: Union[StreamTimeEvent, StreamDepthEvent],
+        context: types.SimpleNamespace,
+    ) -> Any:
+        patches = [
+            mock.patch.object(
+                RawStreamEvent, 'filter_records', return_value=event.records
+            ),
+            mock.patch.object(StreamTimeEvent, 'parse_obj', return_value=event),
+            mock.patch.object(StreamDepthEvent, 'parse_obj', return_value=event),
+        ]
+
+        if isinstance(event, StreamTimeEvent):
+            records = [
+                RawTimeRecord(
+                    asset_id=event.asset_id,  # use asset_id from event to build proper cache key
+                    company_id=int(),
+                    collection=str(),
+                    timestamp=int(),
+                )
+                for record in event.records
+            ]
+
+            metadata = RawMetadata(
+                app_stream_id=int(),
+                apps={SETTINGS.APP_KEY: RawAppMetadata(app_connection_id=int())},
+                log_type=LogType.time,
+            )
+
+            raw_event = [
+                RawStreamTimeEvent(
+                    records=records,
+                    metadata=metadata,
+                ).dict()
+            ]
+
+        if isinstance(event, StreamDepthEvent):
+            records = [
+                RawDepthRecord(
+                    asset_id=event.asset_id,  # use asset_id from event to build proper cache key
+                    company_id=int(),
+                    collection=str(),
+                    measured_depth=float(),
+                )
+                for record in event.records
+            ]
+
+            metadata = RawMetadata(
+                app_stream_id=int(),
+                apps={SETTINGS.APP_KEY: RawAppMetadata(app_connection_id=int())},
+                log_type=LogType.depth,
+            )
+
+            raw_event = [
+                RawStreamDepthEvent(
+                    records=records,
+                    metadata=metadata,
+                ).dict()
+            ]
 
         try:
             [patch.start() for patch in patches]
