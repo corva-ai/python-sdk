31323e6041f14e5977e0576d61364c8d573f407d
Oleksii Symon <oleksii.symon@corva.ai>
2021-03-30 11:29:36 +0300

    reworked tests in test_plugin.py

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -1,238 +1,18 @@
+import contextlib
+
+import pydantic
 import pytest
-import requests_mock as requests_mock_lib
 from pytest_mock import MockerFixture
-from requests_mock import Mocker as RequestsMocker
 
 from corva.application import Corva
-from corva.configuration import SETTINGS
-from corva.models.scheduled import RawScheduledEvent, ScheduledEvent
-from corva.models.stream import StreamEvent
+from corva.models.scheduled import ScheduledEvent
+from corva.models.stream import (
+    StreamDepthEvent,
+    StreamDepthRecord,
+    StreamTimeEvent,
+    StreamTimeRecord,
+)
 from corva.models.task import TaskEvent
-
-
-def app(event, api, cache):
-    return event
-
-
-@pytest.mark.parametrize(
-    'event,expected,raises',
-    (
-        [
-            {
-                "records": [{"asset_id": 0, "timestamp": 0}],
-            },
-            {
-                "records": [{"asset_id": 0, "timestamp": 0}],
-                "metadata": {
-                    'app_stream_id': int(),
-                    'apps': {SETTINGS.APP_KEY: {'app_connection_id': int()}},
-                },
-            },
-            False,
-        ],
-        [
-            [
-                {
-                    "records": [{"asset_id": 0, "timestamp": 0}],
-                }
-            ],
-            {
-                "records": [{"asset_id": 0, "timestamp": 0}],
-                "metadata": {
-                    'app_stream_id': int(),
-                    'apps': {SETTINGS.APP_KEY: {'app_connection_id': int()}},
-                },
-            },
-            False,
-        ],
-        [
-            {
-                "records": [{"asset_id": 0, "timestamp": 0}],
-                "metadata": {
-                    'app_stream_id': 1,
-                    'apps': {SETTINGS.APP_KEY: {'app_connection_id': int()}},
-                },
-            },
-            {
-                "records": [{"asset_id": 0, "timestamp": 0}],
-                "metadata": {
-                    'app_stream_id': 1,
-                    'apps': {SETTINGS.APP_KEY: {'app_connection_id': int()}},
-                },
-            },
-            False,
-        ],
-        [
-            {
-                "records": [{"asset_id": 0, "timestamp": 0}],
-                "metadata": {},
-            },
-            {},
-            True,
-        ],
-    ),
-    ids=[
-        'input event type: dict',
-        'input event type: List[dict]',
-        'metadata can be overwritten by user',
-        'no extra fields added to user metadata',
-    ],
-)
-def test_patch_stream_changes_event(event, expected, raises, corva_context):
-    corva = Corva(corva_context)
-
-    if raises:
-        pytest.raises(Exception, corva.stream, app, event)
-        return
-
-    actual_event = corva.stream(app, event)[0]
-
-    assert actual_event == StreamEvent(**expected)
-
-
-@pytest.mark.parametrize(
-    'event,expected',
-    (
-        [
-            {
-                "schedule": 0,
-                "interval": 0,
-                "schedule_start": 0,
-                "schedule_end": 0,
-                "asset_id": 0,
-            },
-            {
-                "app_connection": 0,
-                "app_stream": 0,
-                "schedule": 0,
-                "interval": 0,
-                "schedule_start": 0,
-                "schedule_end": 0,
-                "asset_id": 0,
-            },
-        ],
-        [
-            [
-                {
-                    "schedule": 0,
-                    "interval": 0,
-                    "schedule_start": 0,
-                    "schedule_end": 0,
-                    "asset_id": 0,
-                }
-            ],
-            {
-                "app_connection": 0,
-                "app_stream": 0,
-                "schedule": 0,
-                "interval": 0,
-                "schedule_start": 0,
-                "schedule_end": 0,
-                "asset_id": 0,
-            },
-        ],
-        [
-            [
-                [
-                    {
-                        "schedule": 0,
-                        "interval": 0,
-                        "schedule_start": 0,
-                        "schedule_end": 0,
-                        "asset_id": 0,
-                    }
-                ]
-            ],
-            {
-                "app_connection": 0,
-                "app_stream": 0,
-                "schedule": 0,
-                "interval": 0,
-                "schedule_start": 0,
-                "schedule_end": 0,
-                "asset_id": 0,
-            },
-        ],
-        [
-            {
-                "app_connection": 1,
-                "schedule": 0,
-                "interval": 0,
-                "schedule_start": 0,
-                "schedule_end": 0,
-                "asset_id": 0,
-            },
-            {
-                "app_connection": 1,
-                "app_stream": 0,
-                "schedule": 0,
-                "interval": 0,
-                "schedule_start": 0,
-                "schedule_end": 0,
-                "asset_id": 0,
-            },
-        ],
-        [
-            {
-                "app_stream": 1,
-                "schedule": 0,
-                "interval": 0,
-                "schedule_start": 0,
-                "schedule_end": 0,
-                "asset_id": 0,
-            },
-            {
-                "app_connection": 0,
-                "app_stream": 1,
-                "schedule": 0,
-                "interval": 0,
-                "schedule_start": 0,
-                "schedule_end": 0,
-                "asset_id": 0,
-            },
-        ],
-    ),
-    ids=[
-        'input event type: dict',
-        'input event type: List[dict]',
-        'input event type: List[List[dict]]',
-        'app_connection can be overwritten by user',
-        'app_stream can be overwritten by user',
-    ],
-)
-def test_patch_scheduled_changes_event(event, expected, corva_context):
-    corva = Corva(corva_context)
-
-    actual_event = corva.scheduled(app, event)[0]
-
-    assert actual_event == RawScheduledEvent(**expected)
-
-
-@pytest.mark.parametrize(
-    '_patch_scheduled,is_patched',
-    ([True, True], [False, False]),
-    indirect=['_patch_scheduled'],
-)
-def test_patch_scheduled_runner_param(
-    _patch_scheduled, is_patched, requests_mock: RequestsMocker, corva_context
-):
-    event = {
-        "schedule": 0,
-        "interval": 0,
-        "schedule_start": 0,
-        "asset_id": 0,
-    }
-
-    corva = Corva(corva_context)
-
-    post_mock = requests_mock.post(requests_mock_lib.ANY)
-
-    corva.scheduled(app, event)
-
-    if is_patched:
-        assert not post_mock.called
-    else:
-        assert post_mock.called_once
 
 
 def test_task_app_runner(app_runner):
@@ -282,3 +62,101 @@ def test_scheduled_app_runner(app_runner):
     event = ScheduledEvent(asset_id=int(), time_from=int(), time_to=int())
 
     assert app_runner(lambda_handler, event) == 'Scheduled app result'
+
+
+@pytest.mark.parametrize(
+    'event',
+    (
+        StreamTimeEvent(
+            asset_id=int(),
+            company_id=int(),
+            records=[StreamTimeRecord(timestamp=int())],
+        ),
+        StreamDepthEvent(
+            asset_id=int(),
+            company_id=int(),
+            records=[StreamDepthRecord(measured_depth=float())],
+        ),
+    ),
+)
+def test_stream_app_runner(event, app_runner):
+    """Should not raise."""
+
+    def lambda_handler(event, context):
+        return Corva(context).stream(
+            fn=lambda event, api, cache: 'Stream app result', event=event
+        )
+
+    assert app_runner(lambda_handler, event) == 'Stream app result'
+
+
+@pytest.mark.parametrize(
+    'event',
+    (
+        StreamTimeEvent(
+            asset_id=1,
+            company_id=int(),
+            records=[StreamTimeRecord(timestamp=int())],
+        ),
+        StreamDepthEvent(
+            asset_id=1,
+            company_id=int(),
+            records=[StreamDepthRecord(measured_depth=float())],
+        ),
+        StreamTimeEvent(
+            asset_id=2,
+            company_id=int(),
+            records=[StreamTimeRecord(timestamp=int())],
+        ),
+        StreamDepthEvent(
+            asset_id=2,
+            company_id=int(),
+            records=[StreamDepthRecord(measured_depth=float())],
+        ),
+    ),
+)
+def test_stream_app_runner_sets_correct_asset_id(
+    event, app_runner, mocker: MockerFixture
+):
+    def lambda_handler(event, context):
+        return Corva(context).stream(fn=lambda event, api, cache: None, event=event)
+
+    # override stream_runner to return event from context
+    mocker.patch('corva.application.stream_runner', lambda fn, context: context.event)
+
+    raw_event = app_runner(lambda_handler, event)
+
+    assert raw_event.asset_id == event.asset_id
+
+
+@pytest.mark.parametrize(
+    'event,exc_ctx',
+    (
+        [
+            StreamTimeEvent(
+                asset_id=int(),
+                company_id=int(),
+                records=[],
+            ),
+            pytest.raises(
+                pydantic.ValidationError,
+                match=r'1 validation error [\s\S]* At least one record should be provided\.',
+            ),
+        ],
+        [
+            StreamTimeEvent(
+                asset_id=int(),
+                company_id=int(),
+                records=[StreamTimeRecord(timestamp=int())],
+            ),
+            contextlib.nullcontext(),
+        ],
+    ),
+    ids=('raises', 'not raises'),
+)
+def test_stream_app_runner_raises_for_empty_records(event, exc_ctx, app_runner):
+    def lambda_handler(event, context):
+        return Corva(context).stream(fn=lambda event, api, cache: None, event=event)
+
+    with exc_ctx:
+        app_runner(lambda_handler, event)
