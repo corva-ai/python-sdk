81a64ebe99aa2bb6e2baa64c5429fd2a0ec74e03
Oleksii Symon <oleksii.symon@corva.ai>
2021-04-16 11:35:54 +0300

    Simplified base_handler

--- src/corva/handlers/base.py
+++ src/corva/handlers/base.py
@@ -1,70 +1,34 @@
 import functools
-from typing import Any, List, Union
+from typing import Any, Callable, List, Type
 
 from corva.api import Api
 from corva.configuration import SETTINGS
-from corva.logger import setup_logging
+from corva.models.base import RawBaseEvent
 from corva.models.context import CorvaContext
-from corva.models.scheduled import RawScheduledEvent
-from corva.models.stream.raw import RawStreamEvent
-from corva.models.task import RawTaskEvent
-from corva.state.redis_state import get_cache
 
 
-def base_handler(func):
-    @functools.wraps(func)
-    def wrapper(
-        event: Union[dict, List[dict], List[List[dict]]], context: Any
-    ) -> List[Any]:
-        context = CorvaContext.parse_obj(context)
+def base_handler(raw_event_type: Type[RawBaseEvent]) -> Callable:
+    def decorator(func: Callable[[RawBaseEvent, Api, str], Any]) -> Callable:
+        @functools.wraps(func)
+        def wrapper(aws_event: Any, aws_context: Any) -> List[Any]:
+            context = CorvaContext.parse_obj(aws_context)
 
-        api = Api(
-            api_url=SETTINGS.API_ROOT_URL,
-            data_api_url=SETTINGS.DATA_API_ROOT_URL,
-            api_key=context.api_key,
-            app_name=SETTINGS.APP_NAME,
-            timeout=None,
-        )
-
-        if isinstance(event, dict):
-            return [
-                func(
-                    event=RawTaskEvent.from_raw_event(event),
-                    api=api,
-                    aws_request_id=context.aws_request_id,
-                )
-            ]
-
-        if any(isinstance(item, dict) for item in event):
-            events = RawStreamEvent.from_raw_event(event)
-        else:
-            events = RawScheduledEvent.from_raw_event(event)
-
-        results = []
-        for event in events:
-            cache = get_cache(
-                asset_id=event.asset_id,
-                app_stream_id=event.app_stream_id,
-                app_connection_id=event.app_connection_id,
-                provider=SETTINGS.PROVIDER,
-                app_key=SETTINGS.APP_KEY,
-                cache_url=SETTINGS.CACHE_URL,
-                cache_settings=None,
+            api = Api(
+                api_url=SETTINGS.API_ROOT_URL,
+                data_api_url=SETTINGS.DATA_API_ROOT_URL,
+                api_key=context.api_key,
+                app_name=SETTINGS.APP_NAME,
+                timeout=None,
             )
 
-            with setup_logging(
-                aws_request_id=context.aws_request_id,
-                asset_id=event.asset_id,
-                app_connection_id=event.app_connection_id,
-            ):
-                results.append(
-                    func(
-                        event=event,
-                        api=api,
-                        cache=cache,
-                    )
-                )
+            raw_events = raw_event_type.from_raw_event(event=aws_event)
 
-        return results
+            results = [
+                func(raw_event, api, context.aws_request_id) for raw_event in raw_events
+            ]
 
-    return wrapper
+            return results
+
+        return wrapper
+
+    return decorator
