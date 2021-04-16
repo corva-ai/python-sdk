324bf3af949141f764cc014f6e97486569b67964
Oleksii Symon <oleksii.symon@corva.ai>
2021-04-16 11:38:25 +0300

    Moved handlers to corva.handlers

--- src/corva/handlers.py
+++ src/corva/handlers.py
@@ -1,14 +1,43 @@
 import contextlib
 import functools
-from typing import Callable
+from typing import Any, Callable, List, Type
 
+from corva import Api
 from corva.api import Api
 from corva.configuration import SETTINGS
-from corva.handlers.base import base_handler
 from corva.logger import setup_logging
+from corva.models.base import RawBaseEvent
+from corva.models.context import CorvaContext
 from corva.models.stream.raw import RawStreamEvent
 from corva.models.stream.stream import StreamEvent
 from corva.state.redis_state import RedisState, get_cache
+
+
+def base_handler(raw_event_type: Type[RawBaseEvent]) -> Callable:
+    def decorator(func: Callable[[RawBaseEvent, Api, str], Any]) -> Callable:
+        @functools.wraps(func)
+        def wrapper(aws_event: Any, aws_context: Any) -> List[Any]:
+            context = CorvaContext.parse_obj(aws_context)
+
+            api = Api(
+                api_url=SETTINGS.API_ROOT_URL,
+                data_api_url=SETTINGS.DATA_API_ROOT_URL,
+                api_key=context.api_key,
+                app_name=SETTINGS.APP_NAME,
+                timeout=None,
+            )
+
+            raw_events = raw_event_type.from_raw_event(event=aws_event)
+
+            results = [
+                func(raw_event, api, context.aws_request_id) for raw_event in raw_events
+            ]
+
+            return results
+
+        return wrapper
+
+    return decorator
 
 
 def stream(func: Callable[[StreamEvent, Api, RedisState], Any]) -> Callable:
