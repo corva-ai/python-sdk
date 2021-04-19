cf504feddd4072f5fbbcee9a90da4f9ecff31283
Oleksii Symon <oleksii.symon@corva.ai>
2021-04-19 15:57:18 +0300

    Deleted obsolete models from corva.models.base

--- src/corva/models/base.py
+++ src/corva/models/base.py
@@ -1,66 +1,19 @@
 from __future__ import annotations
 
-from abc import ABC, abstractmethod
-from typing import Any, Generic, List, Optional, TypeVar, Union
+import abc
+from typing import Any, List
 
-import pydantic.generics
-
-from corva.api import Api
-from corva.configuration import Settings
-from corva.state.redis_state import RedisState, get_cache
-
-
-class EventConfig:
-    extra = pydantic.Extra.allow
-    allow_mutation = False
+import pydantic
 
 
 class CorvaBaseEvent(pydantic.BaseModel):
-    class Config(EventConfig):
-        pass
+    class Config:
+        extra = pydantic.Extra.allow
+        allow_mutation = False
 
 
-class CorvaBaseGenericEvent(pydantic.generics.GenericModel):
-    class Config(EventConfig):
-        pass
-
-
-class RawBaseEvent(ABC):
+class RawBaseEvent(abc.ABC):
     @staticmethod
-    @abstractmethod
+    @abc.abstractmethod
     def from_raw_event(event: Any) -> List[RawBaseEvent]:
         pass
-
-
-RawBaseEventTV = TypeVar('RawBaseEventTV', bound=RawBaseEvent)
-
-
-class BaseContext(pydantic.generics.GenericModel, Generic[RawBaseEventTV]):
-    """Stores common data for running a Corva app."""
-
-    class Config:
-        arbitrary_types_allowed = True
-        extra = pydantic.Extra.ignore
-        validate_assignment = True
-
-    event: RawBaseEventTV
-    settings: Settings
-    api: Api
-    aws_request_id: str
-    cache_: Optional[RedisState] = None
-    cache_settings: dict = {}
-
-    @property
-    def cache(self) -> RedisState:
-        if self.cache_:
-            return self.cache_
-
-        self.cache_ = get_cache(
-            asset_id=self.event.asset_id,
-            app_stream_id=self.event.app_stream_id,
-            app_connection_id=self.event.app_connection_id,
-            settings=self.settings,
-            cache_settings=self.cache_settings,
-        )
-
-        return self.cache_
