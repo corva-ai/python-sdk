9082183a3fb3f508505038c3d06356d712802a00
Oleksii Symon <oleksii.symon@corva.ai>
2021-04-27 13:24:21 +0300

    Added documentation version to index.adoc

---
+++ tests/test_docs/test_version.py
@@ -0,0 +1,13 @@
+import pathlib
+import re
+
+import version
+
+
+def test_doc_and_lib_versions_match():
+    doc_text = pathlib.Path('docs/index.adoc').read_text()
+    # look for text like: Documentation for version: *v0.0.18*
+    doc_match = re.compile(r'Documentation for version: \*v.*\..*\..*').search(doc_text)
+    doc_version = doc_match.group().strip('.*').split('v')[-1]
+
+    assert version.VERSION == doc_version
