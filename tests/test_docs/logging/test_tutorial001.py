2aec8916ee6014d0ca2d08ccf6d99c797fcc8e52
Oleksii Symon <oleksii.symon@corva.ai>
2021-04-27 11:48:50 +0300

    Fixed test in logging/test_tutorial001.py

--- tests/test_docs/logging/test_tutorial001.py
+++ tests/test_docs/logging/test_tutorial001.py
@@ -1,24 +1,29 @@
-import inspect
+import logging
 
+from pytest_mock import MockerFixture
+
+from corva import Logger
 from corva.models.task import TaskEvent
 from docs.src.logging import tutorial001
 
 
-def test_tutorial001(app_runner, capfd):
+def test_tutorial001(app_runner, mocker: MockerFixture, capsys):
     event = TaskEvent(asset_id=0, company_id=0)
+
+    Logger.setLevel('DEBUG')
+
+    # Add handler, as Logger doesnt have one by default
+    handler = logging.StreamHandler()
+    handler.setLevel('DEBUG')
+    mocker.patch.object(Logger, 'handlers', [handler])
 
     app_runner(tutorial001.task_app, event)
 
-    expected = inspect.cleandoc(
-        """
-    Warning message!
-    Error message!
-    Exception message!
-    Traceback (most recent call last):
-      File "/home/oleksii/Corva/python-sdk/docs/src/logging/tutorial001.py", line 13, in task_app
-        0 / 0
-    ZeroDivisionError: division by zero
-    """
+    expected = (
+        'Debug message!\nInfo message!\nWarning message!\n'
+        'Error message!\nException message!\n'
     )
 
-    assert inspect.cleandoc(capfd.readouterr().err) == expected
+    captured = capsys.readouterr().err
+
+    assert captured.startswith(expected)
