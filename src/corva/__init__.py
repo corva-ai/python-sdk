e13df1ca72f65a846c90642a086d8a84e8b49ef8
Oleksii Symon <oleksii.symon@corva.ai>
2021-08-06 13:37:18 +0300

    Added deprecation warning for `corva.ScheduledEvent` usage

--- src/corva/__init__.py
+++ src/corva/__init__.py
@@ -15,19 +15,19 @@ from .models.stream.stream import (
 from .models.task import TaskEvent
 from .state.redis_state import RedisState as Cache
 
-__all__ = [
-    'Api',
-    'Cache',
-    'Logger',
-    'ScheduledDepthEvent',
-    'ScheduledNaturalTimeEvent',
-    'ScheduledDataTimeEvent',
-    'StreamDepthEvent',
-    'StreamDepthRecord',
-    'StreamTimeEvent',
-    'StreamTimeRecord',
-    'TaskEvent',
-    'scheduled',
-    'stream',
-    'task',
-]
+
+def __getattr__(name):
+    import warnings
+
+    if name == "ScheduledEvent":
+        warnings.warn(
+            "The corva.ScheduledEvent class is deprecated "
+            "and will be removed from corva in the next major version. "
+            "Import corva.ScheduledDataTimeEvent instead.",
+            FutureWarning,
+            stacklevel=2,
+        )
+
+        return ScheduledDataTimeEvent
+
+    raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
