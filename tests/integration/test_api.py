bca7f5a305ac101df9a664742c362f7549c3b55f
Oleksii Symon <oleksii.symon@corva.ai>
2022-05-10 11:14:35 +0300

    Lint fix

--- tests/integration/test_api.py
+++ tests/integration/test_api.py
@@ -1,9 +1,9 @@
-import inspect
 import logging
 
 import httpx
 import pytest
 import pytest_httpx
+import yaml
 
 import corva.api_adapter
 import corva.configuration
@@ -11,6 +11,8 @@ import corva.configuration
 
 class TestLogsFailedRequests:
     def test_no_response(self, caplog: pytest.LogCaptureFixture):
+        caplog.handler.setFormatter(logging.Formatter('%(message)s'))
+
         sdk = corva.api_adapter.UserApiSdk(
             platform_api_url='',
             data_api_url='',
@@ -22,29 +24,32 @@ class TestLogsFailedRequests:
         with sdk as s, pytest.raises(httpx.HTTPError):
             s.data.v1.http.get(url='whatever')
 
-        actual = caplog.text.strip()
-        expected = inspect.cleandoc(
-            """
-            ERROR    root:api_adapter.py:62 message: Request failed - Request URL is missing an 'http://' or 'https://' protocol.
-            request:
-              method: GET
-              url: api/v1/whatever
-              headers:
-                accept: '*/*'
-                accept-encoding: gzip, deflate
-                connection: keep-alive
-                user-agent: python-httpx/0.22.0
-                authorization: '[secure]'
-                x-corva-app: ''
-              content: b''
-              """
-        ).strip()
+        actual = yaml.safe_load(caplog.text)
+
+        expected = {
+            'message': "Request failed - Request URL is missing an 'http://' or "
+            "'https://' protocol.",
+            'request': {
+                'method': 'GET',
+                'url': 'api/v1/whatever',
+                'headers': {
+                    'accept': '*/*',
+                    'accept-encoding': 'gzip, deflate',
+                    'connection': 'keep-alive',
+                    'user-agent': 'python-httpx/0.22.0',
+                    'authorization': '[secure]',
+                    'x-corva-app': '',
+                },
+                'content': "b''",
+            },
+        }
 
         assert expected == actual
 
     def test_unsuccessful_response(
         self, caplog: pytest.LogCaptureFixture, httpx_mock: pytest_httpx.HTTPXMock
     ):
+        caplog.handler.setFormatter(logging.Formatter('%(message)s'))
         httpx_mock.add_response(status_code=400)
 
         sdk = corva.api_adapter.UserApiSdk(
@@ -58,30 +63,32 @@ class TestLogsFailedRequests:
         with sdk as s:
             s.data.v1.http.get(url='')
 
-        actual = caplog.text.strip()
-        expected = inspect.cleandoc(
-            """
-            ERROR    root:api_adapter.py:71 message: 'Request failed - Client error ''400 Bad Request'' for url ''https://test_url/api/v1/''
+        actual = yaml.safe_load(caplog.text)
 
-              For more information check: https://httpstatuses.com/400'
-            response:
-              code: 400
-              reason: Bad Request
-              headers: {}
-              content: b''
-            request:
-              method: GET
-              url: https://test_url/api/v1/
-              headers:
-                host: test_url
-                accept: '*/*'
-                accept-encoding: gzip, deflate
-                connection: keep-alive
-                user-agent: python-httpx/0.22.0
-                authorization: '[secure]'
-                x-corva-app: ''
-              content: b''
-            """
-        ).strip()
+        expected = {
+            'message': "Request failed - Client error '400 Bad Request' for url "
+            "'https://test_url/api/v1/'\nFor more information check: "
+            "https://httpstatuses.com/400",
+            'response': {
+                'code': 400,
+                'reason': 'Bad Request',
+                'headers': {},
+                'content': "b''",
+            },
+            'request': {
+                'method': 'GET',
+                'url': 'https://test_url/api/v1/',
+                'headers': {
+                    'host': 'test_url',
+                    'accept': '*/*',
+                    'accept-encoding': 'gzip, deflate',
+                    'connection': 'keep-alive',
+                    'user-agent': 'python-httpx/0.22.0',
+                    'authorization': '[secure]',
+                    'x-corva-app': '',
+                },
+                'content': "b''",
+            },
+        }
 
         assert actual == expected
