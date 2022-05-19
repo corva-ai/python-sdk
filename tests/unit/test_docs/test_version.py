0ea3bfd62a05877614542274214e05967db54ceb
Oleksii Symon <oleksii.symon@corva.ai>
2022-05-19 14:11:59 +0300

    Mypy fix #1

--- tests/unit/test_docs/test_version.py
+++ tests/unit/test_docs/test_version.py
@@ -1,6 +1,8 @@
 import pathlib
 import re
 
+import pytest
+
 import version
 
 
@@ -8,6 +10,10 @@ def test_doc_and_lib_versions_match():
     doc_text = pathlib.Path('docs/index.adoc').read_text()
     # searches text like: Documentation for version *v0.0.18*
     doc_match = re.compile(r'Documentation for version \*v.*\..*\..*').search(doc_text)
+
+    if doc_match is None:
+        pytest.fail('Could not find documentation version.')
+
     doc_version = doc_match.group().strip('.*').split('v')[-1]
 
     assert version.VERSION == doc_version
