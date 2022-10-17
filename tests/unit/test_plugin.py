4c80e18791fc1d5b2c284fe83cedc9bec47f0afc
Mo Kamyab <m.kamyab@corva.ai>
2022-10-17 09:16:14 -0500

    added the functionality to reuse cache in app_runner

--- tests/unit/test_plugin.py
+++ tests/unit/test_plugin.py
@@ -1,5 +1,6 @@
 import pytest
 
+from corva.configuration import SETTINGS
 from corva.handlers import scheduled, stream, task
 from corva.models.scheduled.scheduled import (
     ScheduledDataTimeEvent,
@@ -13,6 +14,7 @@ from corva.models.stream.stream import (
     StreamTimeRecord,
 )
 from corva.models.task import TaskEvent
+from corva.service.cache_sdk import UserRedisSdk
 
 
 def test_task_app_runner(app_runner):
@@ -85,3 +87,37 @@ def test_stream_app_runner(event, app_runner):
         return 'Stream app result'
 
     assert app_runner(stream_app, event) == 'Stream app result'
+
+
+def test_reuse_cache(event, app_runner):
+    """
+    Testing that cache is reset or reused between runs.
+    """
+    @scheduled
+    def scheduled_fibonacci(event, api, cache):
+        number1 = int(cache.get('number1') or 1)
+        number2 = int(cache.get('number2') or 1)
+        number3 = number1 + number2
+        cache.set('number1', number2)
+        cache.set('number2', number3)
+
+        return number3
+
+    event = ScheduledDataTimeEvent(
+        asset_id=0, company_id=0,
+        start_time=0, end_time=0
+    )
+
+    # resetting cache - so the results should be the same
+    for _ in range(5):
+        assert app_runner(scheduled_fibonacci, event) == 2
+
+    # reusing cache
+    cache = UserRedisSdk(
+        hash_name='hash_name',
+        redis_dsn=SETTINGS.CACHE_URL,
+        use_fakes=True,
+    )
+    expected_results = [2, 3, 5, 8, 13, 21, 34, 55]
+    for expected_result in expected_results:
+        assert app_runner(scheduled_fibonacci, event, cache=cache) == expected_result
