4b0db6b40312bd2d8f15f370d05041a740073422
Alex Skyrta <oleksandr.skyrta@corva.ai>
2023-10-25 13:44:39 +0300

    Fix linter

--- tests/unit/test_manual_cache_init.py
+++ tests/unit/test_manual_cache_init.py
@@ -1,21 +1,16 @@
-from corva.service.cache_sdk import UserRedisSdk, InternalRedisSdk
 import pytest
+
+from corva.service.cache_sdk import InternalRedisSdk, UserRedisSdk
 
 
 @pytest.mark.parametrize(
     'cache_class',
-    (
-        UserRedisSdk,
-        InternalRedisSdk
-    ),
+    (UserRedisSdk, InternalRedisSdk),
 )
 def test_manual_cache_init(cache_class):
     """
-    When redis_client is not provided our cache classes need to initialize client by themselves
+    When redis_client is not provided our cache classes need to initialize
+    client by themselves
     """
-    cache = cache_class(
-        "test",
-        "redis://localhost:6379",
-        redis_client=None
-    )
+    cache = cache_class("test", "redis://localhost:6379", redis_client=None)
     assert cache.cache_repo.client is not None
