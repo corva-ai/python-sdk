55a656b9ecf9e915663693b8deae30810a88df53
Alex Skyrta <oleksandr.skyrta@corva.ai>
2023-12-01 13:26:14 +0200

    Updated documentation

---
+++ docs/modules/ROOT/examples/merging/tutorial001.py
@@ -0,0 +1,9 @@
+from corva import Api, StreamTimeEvent, stream
+
+
+# imagine we actually have 3 incoming events with 3 records each
+@stream(merge_events=True)
+def task_app(event: StreamTimeEvent, api: Api):
+    # since we passed merge_events=True all 3 incoming events
+    # and their records will be merged into a single event with 9 records
+    assert len(event.records) == 9  # this will not fail
